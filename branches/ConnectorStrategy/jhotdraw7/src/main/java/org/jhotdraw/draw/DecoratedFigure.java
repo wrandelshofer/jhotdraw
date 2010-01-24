da18a881daf3fdc390c2088950d7bb15a5a06c18
cfm1 <cfm1@fc158eef-6c16-0410-b9c5-873085b46621>
2010-01-24 03:53:29 +0000

    Branch for ConnectorStrategy

---
+++ branches/ConnectorStrategy/jhotdraw7/src/main/java/org/jhotdraw/draw/DecoratedFigure.java
@@ -0,0 +1,53 @@
+/*
+ * @(#)DecoratedFigure.java
+ *
+ * Copyright (c) 1996-2010 by the original authors of JHotDraw
+ * and all its contributors.
+ * All rights reserved.
+ *
+ * The copyright of this software is owned by the authors and  
+ * contributors of the JHotDraw project ("the copyright holders").  
+ * You may not use, copy or modify this software, except in  
+ * accordance with the license agreement you entered into with  
+ * the copyright holders. For details see accompanying license terms. 
+ */
+
+package org.jhotdraw.draw;
+
+import org.jhotdraw.draw.*;
+
+/**
+ * A <em>decorated figure</em> can be decorated with another {@link Figure},
+ * for example to draw a border around the decorated figure.
+ *
+ * <hr>
+ * <b>Design Patterns</b>
+ *
+ * <p><em>Decorator</em><br>
+ * Decorated figures can be adorned with another figure.<br>
+ * Component: {@link DecoratedFigure}; Decorator: {@link Figure}.
+ *
+ * <p><em>Strategy</em><br>
+ * {@code RelativeDecoratorLocator} encapsulates a strategy for locating a
+ * point on a decorator.<br>
+ * Component: {@link DecoratedFigure}; Strategy: {@link org.jhotdraw.draw.locator.RelativeDecoratorLocator}.
+ * 
+ * <hr>
+ *
+ * @author Werner Randelshofer
+ * @version $Id$
+ */
+public interface DecoratedFigure extends Figure {
+    /**
+     * Sets a decorator Figure, for example a visual adornment to this Figure.
+     * Set this to null, if no decorator is desired.
+     * The decorator uses the same logical bounds as this Figure plus 
+     * AttributeKeys.DECORATOR_INSETS. The decorator does not handle events.
+     * The decorator is drawn when the figure is drawn.
+     */
+    public void setDecorator(Figure newValue);
+    /**
+     * Gets the decorator for this figure.
+     */
+    public Figure getDecorator();    
+}
---
+++ branches/ConnectorStrategy/jhotdraw7/src/main/java/org/jhotdraw/samples/teddy/Main.java
@@ -0,0 +1,53 @@
+/*
+ * @(#)Main.java
+ *
+ * Copyright (c) 1996-2010 by the original authors of JHotDraw
+ * and all its contributors.
+ * All rights reserved.
+ *
+ * The copyright of this software is owned by the authors and  
+ * contributors of the JHotDraw project ("the copyright holders").  
+ * You may not use, copy or modify this software, except in  
+ * accordance with the license agreement you entered into with  
+ * the copyright holders. For details see accompanying license terms. 
+ */
+
+package org.jhotdraw.samples.teddy;
+
+import org.jhotdraw.app.*;
+
+/**
+ * Main class.
+ *
+ * @author Werner Randelshofer.
+ * @version $Id$
+ */
+public class Main {
+    public final static String NAME = "JHotDraw Teddy";
+    public final static String COPYRIGHT = "© 1996-2010 by the original authors of JHotDraw and all its contributors.";
+    
+    /**
+     * Launches the application.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TeddyApplicationModel tam = new TeddyApplicationModel();
+        tam.setCopyright(COPYRIGHT);
+        tam.setName(NAME);
+        tam.setViewClassName("org.jhotdraw.samples.teddy.TeddyView");
+        tam.setVersion(Main.class.getPackage().getImplementationVersion());
+        
+        Application app;
+        if (System.getProperty("os.name").toLowerCase().startsWith("mac os x")) {
+            app = new OSXApplication();
+        } else if (System.getProperty("os.name").toLowerCase().startsWith("win")) {
+            //app = new DefaultMDIApplication();
+            app = new SDIApplication();
+        } else {
+            app = new SDIApplication();
+        }
+        app.setModel(tam);
+        app.launch(args);
+    }
+}
