13cf21940d7d0604642faba7c02760140194a1a2
rawcoder <werner.randelshofer@fibermail.ch>
2015-09-29 06:17:02 +0000

    Fixes inheritance of Key's for TextHolder, TextFigure, LabelFigure.

--- jhotdraw8/src/main/java/org/jhotdraw/draw/SimpleLabelFigure.java
+++ jhotdraw8/src/main/java/org/jhotdraw/draw/SimpleLabelFigure.java
@@ -2,8 +2,12 @@
  * Copyright (c) 2015 by the authors and contributors of JHotDraw.
  * You may only use this file in compliance with the accompanying license terms.
  */
-package org.jhotdraw.draw.shape;
+package org.jhotdraw.draw;
 
+import org.jhotdraw.draw.key.DirtyBits;
+import org.jhotdraw.draw.key.DirtyMask;
+import org.jhotdraw.draw.key.SimpleFigureKey;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.geometry.BoundingBox;
 import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
@@ -12,14 +16,7 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextBoundsType;
 import javafx.scene.transform.Transform;
 import org.jhotdraw.collection.Key;
-import org.jhotdraw.draw.key.DirtyBits;
-import org.jhotdraw.draw.key.DirtyMask;
-import org.jhotdraw.draw.Figure;
-import org.jhotdraw.draw.key.SimpleFigureKey;
-import org.jhotdraw.draw.TextHolderFigure;
-import org.jhotdraw.draw.connector.ChopRectangleConnector;
 import org.jhotdraw.draw.connector.Connector;
-import org.jhotdraw.draw.RenderContext;
 
 /**
  * TextFigure.
@@ -27,26 +24,27 @@ import org.jhotdraw.draw.RenderContext;
  * @author Werner Randelshofer
  * @version $Id$
  */
-public class TextFigure extends AbstractShapeFigure implements TextHolderFigure {
+public class SimpleLabelFigure extends AbstractLeafFigure implements TextHolderFigure, LabelFigure {
 
+    public final static Key<Point2D> ORIGIN = new SimpleFigureKey<>("origin", Point2D.class, DirtyMask.of(DirtyBits.NODE),new Point2D(0, 0));
     /**
-     * The CSS type selector for this object is {@code "Text"}.
+     * The CSS type selector for a label object is {@code "Label"}.
      */
-    public final static String TYPE_SELECTOR = "Text";
+    public final static String TYPE_SELECTOR = "Label";
 
-    public final static SimpleFigureKey<Point2D> ORIGIN = new SimpleFigureKey<>("origin", Point2D.class, false, DirtyMask.of(DirtyBits.NODE, DirtyBits.CONNECTION_LAYOUT, DirtyBits.LAYOUT), new Point2D(0, 0));
+    private ReadOnlyObjectWrapper<Bounds> layoutBounds = null;
 
     private Text textNode;
 
-    public TextFigure() {
+    public SimpleLabelFigure() {
         this(0, 0, "");
     }
 
-    public TextFigure(Point2D position, String text) {
+    public SimpleLabelFigure(Point2D position, String text) {
         this(position.getX(), position.getY(), text);
     }
 
-    public TextFigure(double x, double y, String text) {
+    public SimpleLabelFigure(double x, double y, String text) {
         set(TEXT, text);
         set(ORIGIN, new Point2D(x, y));
     }
@@ -70,34 +68,39 @@ public class TextFigure extends AbstractShapeFigure implements TextHolderFigure 
     }
 
     @Override
-    public void reshape(double x, double y, double width, double height) {
-        set(ORIGIN, new Point2D(x, y));
-    }
-
-    @Override
     public Node createNode(RenderContext drawingView) {
         return new Text();
     }
 
     @Override
     public void updateNode(RenderContext drawingView, Node node) {
-        Text tn = (Text) node;
-        tn.setText(get(TEXT));
-        tn.setX(get(ORIGIN).getX());
-        tn.setY(get(ORIGIN).getY());
-        tn.setBoundsType(TextBoundsType.VISUAL);
-        applyTextProperties(tn);
-        applyShapeProperties(tn);
-        tn.applyCss();
+        Text textNode = (Text) node;
+        textNode.setText(get(TEXT));
+        textNode.setX(get(ORIGIN).getX());
+        textNode.setY(get(ORIGIN).getY());
+        textNode.setBoundsType(TextBoundsType.VISUAL);
+        applyTextProperties(textNode);
+        applyLabelProperties(textNode);
+    }
+
+    @Override
+    public boolean isLayoutable() {
+        return false;
     }
 
     @Override
     public Connector findConnector(Point2D p, Figure prototype) {
-        return new ChopRectangleConnector();
+        return null;
+    }
+
+    @Override
+    public void layout() {
+        // empty!
     }
 
     @Override
     public String getTypeSelector() {
         return TYPE_SELECTOR;
     }
+
 }
