5adf8386963c982efa5c0ae9c1b99176ea7a2dde
rawcoder <werner.randelshofer@fibermail.ch>
2015-10-18 16:05:43 +0000

    Adds new scanner,  tokenizer and AST classes which will be used by a future CSS parser. Renames the existing CSS parser to "CssParserOld".

--- jhotdraw8/src/main/java/org/jhotdraw/xml/css/package-info.java
+++
@@ -1,8 +0,0 @@
-/* @(#)package-info.java
- * Copyright (c) 2015 by the authors and contributors of JHotDraw.
- * You may only use this file in compliance with the accompanying license terms.
- */
-/**
-Provides support for cascading style sheets (CSS) in XML DOM.
-*/
-package org.jhotdraw.xml.css;
