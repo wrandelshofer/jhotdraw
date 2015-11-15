48845d1548b5c2814601f5892f2cc0f0c608c204
rawcoder <rawcoder@fc158eef-6c16-0410-b9c5-873085b46621>
2015-11-15 11:23:59 +0000

    A LabelFigure does not have to be transformable.

--- trunk/jhotdraw8/src/main/java/org/jhotdraw/draw/LabelFigure.java
+++ trunk/jhotdraw8/src/main/java/org/jhotdraw/draw/LabelFigure.java
@@ -7,13 +7,6 @@ package org.jhotdraw.draw;
 
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
-import static org.jhotdraw.draw.TextHolderFigure.FONT;
-import static org.jhotdraw.draw.TextHolderFigure.LINE_SPACING;
-import static org.jhotdraw.draw.TextHolderFigure.STRIKETHROUGH;
-import static org.jhotdraw.draw.TextHolderFigure.TEXT_ALIGNMENT;
-import static org.jhotdraw.draw.TextHolderFigure.TEXT_ORIGIN;
-import static org.jhotdraw.draw.TextHolderFigure.UNDERLINE;
-import static org.jhotdraw.draw.TextHolderFigure.WRAPPING_WIDTH;
 import org.jhotdraw.draw.key.PaintStyleableFigureKey;
 
 /**
