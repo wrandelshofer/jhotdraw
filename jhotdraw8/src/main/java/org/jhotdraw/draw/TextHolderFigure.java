5f16d6f93e757f55eafcdd6c74049f12480a7a5f
rawcoder <werner.randelshofer@fibermail.ch>
2015-11-15 18:28:57 +0000

    Factors Keys out of the Figure interface into multiple interfaces.
    Fixes a number of CSS bugs.

--- jhotdraw8/src/main/java/org/jhotdraw/draw/TextHolderFigure.java
+++ jhotdraw8/src/main/java/org/jhotdraw/draw/TextHolderFigure.java
@@ -1,75 +1,14 @@
-/*
- * @(#)TextHolderFigure.java
+/* @(#)TextHolderFigure.java
  * Copyright (c) 2015 by the authors and contributors of JHotDraw.
  * You may only use this file in compliance with the accompanying license terms.
  */
-package org.jhotdraw.draw;
 
-import org.jhotdraw.draw.key.DirtyBits;
-import org.jhotdraw.draw.key.DirtyMask;
-import org.jhotdraw.draw.key.SimpleFigureKey;
-import javafx.geometry.VPos;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-import org.jhotdraw.draw.key.BooleanStyleableFigureKey;
-import org.jhotdraw.draw.key.DoubleStyleableFigureKey;
-import org.jhotdraw.draw.key.EnumStyleableFigureKey;
-import org.jhotdraw.draw.key.FontStyleableFigureKey;
+package org.jhotdraw.draw;
 
 /**
  * TextHolderFigure.
- *
  * @author Werner Randelshofer
- * @version $Id$
  */
-public interface TextHolderFigure extends Figure {
+public class TextHolderFigure {
 
-    // text properties
-    /**
-     * Defines the font used. Default value: {@code new Font("System",12)}
-     */
-    public static FontStyleableFigureKey FONT = new FontStyleableFigureKey("font", new Font("System", 12.0));
-    /**
-     * The line spacing. Default value: {@code 12.0}
-     */
-    public static DoubleStyleableFigureKey LINE_SPACING = new DoubleStyleableFigureKey("lineSpacing", DirtyMask.of(DirtyBits.NODE, DirtyBits.CONNECTION_LAYOUT, DirtyBits.LAYOUT), 12.0);
-    /**
-     * Whether to strike through the text. Default value: {@code false}
-     */
-    public static BooleanStyleableFigureKey STRIKETHROUGH = new BooleanStyleableFigureKey("strikethrough", DirtyMask.of(DirtyBits.NODE), false);
-    /**
-     * The text alignment. Default value: {@code left}
-     */
-    public static EnumStyleableFigureKey<TextAlignment> TEXT_ALIGNMENT = new EnumStyleableFigureKey<>("textAlignment", TextAlignment.class, DirtyMask.of(DirtyBits.NODE, DirtyBits.CONNECTION_LAYOUT), TextAlignment.LEFT);
-    /**
-     * The text origin. Default value: {@code baseline}
-     */
-    public static EnumStyleableFigureKey<VPos> TEXT_ORIGIN = new EnumStyleableFigureKey<>("textOrigin", VPos.class, DirtyMask.of(DirtyBits.NODE, DirtyBits.CONNECTION_LAYOUT), VPos.BASELINE);
-    /**
-     * Whether to underline the text. Default value: {@code false}
-     */
-    public static BooleanStyleableFigureKey UNDERLINE = new BooleanStyleableFigureKey("underline", DirtyMask.of(DirtyBits.NODE), false);
-    /**
-     * Text wrapping width. Default value: {@code 0.0} (no wrapping).
-     */
-    public static DoubleStyleableFigureKey WRAPPING_WIDTH = new DoubleStyleableFigureKey("wrappingWidth", DirtyMask.of(DirtyBits.NODE, DirtyBits.CONNECTION_LAYOUT, DirtyBits.LAYOUT), 0.0);
-
-    /** The text. Default value: {@code ""}. */
-    public final static SimpleFigureKey<String> TEXT = new SimpleFigureKey<>("text", String.class, DirtyMask.of(DirtyBits.NODE, DirtyBits.CONNECTION_LAYOUT, DirtyBits.LAYOUT), "");
-
-    /**
-     * Updates a text node with text properties except {@code TEXT}.
-     *
-     * @param text a text node
-     */
-    default void applyTextHolderProperties(Text text) {
-        text.setFont(getStyled(FONT));
-        text.setLineSpacing(getStyled(LINE_SPACING));
-        text.setStrikethrough(getStyled(STRIKETHROUGH));
-        text.setTextAlignment(getStyled(TEXT_ALIGNMENT));
-        text.setTextOrigin(getStyled(TEXT_ORIGIN));
-        text.setUnderline(getStyled(UNDERLINE));
-        text.setWrappingWidth(getStyled(WRAPPING_WIDTH));
-    }
 }
