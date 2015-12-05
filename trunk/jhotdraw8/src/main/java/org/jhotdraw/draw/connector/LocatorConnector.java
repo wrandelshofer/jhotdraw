6e41bbff728bfccb7fe5f733cfe489b6304b6300
rawcoder <rawcoder@fc158eef-6c16-0410-b9c5-873085b46621>
2015-12-05 09:54:59 +0000

    Adds LocatorConnector. Improves comments in Layer.

---
+++ trunk/jhotdraw8/src/main/java/org/jhotdraw/draw/connector/LocatorConnector.java
@@ -0,0 +1,56 @@
+/*
+ * @(#)LocatorConnector.java
+ * Copyright (c) 2014 Supercomputing Systems AG, Schweiz.
+ * Alle Rechte vorbehalten. 
+ */
+package org.jhotdraw.draw.connector;
+
+import javafx.geometry.Bounds;
+import javafx.geometry.Point2D;
+import org.jhotdraw.draw.Figure;
+import org.jhotdraw.draw.locator.Locator;
+
+/**
+ * LocatorConnector uses a {@link Locator} to compute its position.
+ *
+ * @author Werner Randelshofer
+ * @version $$Id$$
+ */
+public class LocatorConnector extends AbstractConnector {
+
+    private Locator locator;
+
+    /**
+     * Creates a new instance
+     *
+     * @param target the target figure
+     * @param locator the locator that should be used
+     */
+    public LocatorConnector(Figure target, Locator locator) {
+        super(target);
+        this.locator = locator;
+    }
+
+    /** Returns the locator used to compute the position of the connector.
+     * 
+     * @return the locator
+     */
+    public Locator getLocator() {
+        return locator;
+    }
+
+    public void setLocator(Locator locator) {
+        this.locator = locator;
+    }
+
+    @Override
+    public Point2D getPositionInLocal(Figure connection) {
+        final Bounds b = target.getBoundsInLocal();
+        return locator.locate(target);
+    }
+
+    @Override
+    public Point2D chopStart(Figure connection, double startX, double startY, double endX, double endY) {
+        return getPositionInDrawing(connection);
+    }
+}
