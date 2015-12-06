b9f5c63d482445da72ccf4ba00dfb60e39b82ac7
rawcoder <werner.randelshofer@fibermail.ch>
2015-12-06 11:40:38 +0000

    Adds Xml converters.

---
+++ jhotdraw8/src/main/java/org/jhotdraw/text/XmlRegexConverter.java
@@ -0,0 +1,14 @@
+/* @(#)XmlRegexConverter.java
+ * Copyright (c) 2015 by the authors and contributors of JHotDraw.
+ * You may only use this file in compliance with the accompanying license terms.
+ */
+
+package org.jhotdraw.text;
+
+/**
+ * XmlRegexConverter.
+ * @author Werner Randelshofer
+ */
+public class XmlRegexConverter extends CssRegexConverter {
+
+}
