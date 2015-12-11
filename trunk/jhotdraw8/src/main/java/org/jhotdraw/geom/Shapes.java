370d19b287050e4a3b86d03e120985cbdc7166da
rawcoder <rawcoder@fc158eef-6c16-0410-b9c5-873085b46621>
2015-12-11 19:04:22 +0000

    Improves SVG export.

--- trunk/jhotdraw8/src/main/java/org/jhotdraw/geom/Shapes.java
+++ trunk/jhotdraw8/src/main/java/org/jhotdraw/geom/Shapes.java
@@ -9,16 +9,38 @@ package org.jhotdraw.geom;
 
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.FlatteningPathIterator;
+import java.awt.geom.Line2D;
+import java.awt.geom.Path2D;
 import java.awt.geom.PathIterator;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
+import java.io.IOException;
 import java.util.List;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcTo;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.ClosePath;
+import javafx.scene.shape.CubicCurve;
 import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.HLineTo;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.scene.shape.PathElement;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Polyline;
+import javafx.scene.shape.QuadCurve;
 import javafx.scene.shape.QuadCurveTo;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.SVGPath;
+import javafx.scene.shape.VLineTo;
+import javafx.scene.text.Text;
 import javafx.scene.transform.MatrixType;
 
 /**
@@ -36,8 +58,7 @@ public class Shapes {
     }
 
     /**
-     * Returns true, if the outline of this bezier path contains the specified
-     * point.
+     * Returns true, if the outline of this shape contains the specified point.
      *
      * @param shape The shape.
      * @param p The point to be tested.
@@ -50,30 +71,30 @@ public class Shapes {
         double moveX = 0, moveY = 0;
         for (PathIterator i = new FlatteningPathIterator(shape.getPathIterator(new AffineTransform(), tolerance), tolerance); !i.isDone(); i.next()) {
             switch (i.currentSegment(coords)) {
-            case PathIterator.SEG_CLOSE:
-                if (Geom.lineContainsPoint(
-                        prevX, prevY, moveX, moveY,
-                        p.x, p.y, tolerance)) {
-                    return true;
-                }
-                break;
-            case PathIterator.SEG_CUBICTO:
-                break;
-            case PathIterator.SEG_LINETO:
-                if (Geom.lineContainsPoint(
-                        prevX, prevY, coords[0], coords[1],
-                        p.x, p.y, tolerance)) {
-                    return true;
-                }
-                break;
-            case PathIterator.SEG_MOVETO:
-                moveX = coords[0];
-                moveY = coords[1];
-                break;
-            case PathIterator.SEG_QUADTO:
-                break;
-            default:
-                break;
+                case PathIterator.SEG_CLOSE:
+                    if (Geom.lineContainsPoint(
+                            prevX, prevY, moveX, moveY,
+                            p.x, p.y, tolerance)) {
+                        return true;
+                    }
+                    break;
+                case PathIterator.SEG_CUBICTO:
+                    break;
+                case PathIterator.SEG_LINETO:
+                    if (Geom.lineContainsPoint(
+                            prevX, prevY, coords[0], coords[1],
+                            p.x, p.y, tolerance)) {
+                        return true;
+                    }
+                    break;
+                case PathIterator.SEG_MOVETO:
+                    moveX = coords[0];
+                    moveY = coords[1];
+                    break;
+                case PathIterator.SEG_QUADTO:
+                    break;
+                default:
+                    break;
             }
             prevX = coords[0];
             prevY = coords[1];
@@ -88,11 +109,12 @@ public class Shapes {
      * @return An AWT Transform.
      */
     public static AffineTransform awtTransformFromFX(javafx.scene.transform.Transform fxT) {
-        if (fxT==null)
+        if (fxT == null) {
             return null;
-        
-        double[] m=fxT.toArray(MatrixType.MT_2D_2x3);
-        return fxT == null ? null : new AffineTransform(m[0],m[3],m[1],m[4],m[2],m[5]);
+        }
+
+        double[] m = fxT.toArray(MatrixType.MT_2D_2x3);
+        return fxT == null ? null : new AffineTransform(m[0], m[3], m[1], m[4], m[2], m[5]);
     }
 
     /**
@@ -127,38 +149,239 @@ public class Shapes {
         javafx.scene.shape.Path fxpath = new javafx.scene.shape.Path();
 
         switch (iter.getWindingRule()) {
-        case PathIterator.WIND_EVEN_ODD:
-            fxpath.setFillRule(javafx.scene.shape.FillRule.EVEN_ODD);
-            break;
-        case PathIterator.WIND_NON_ZERO:
-            fxpath.setFillRule(javafx.scene.shape.FillRule.NON_ZERO);
-            break;
-        default:
-            throw new IllegalArgumentException("illegal winding rule " + iter.getWindingRule());
+            case PathIterator.WIND_EVEN_ODD:
+                fxpath.setFillRule(javafx.scene.shape.FillRule.EVEN_ODD);
+                break;
+            case PathIterator.WIND_NON_ZERO:
+                fxpath.setFillRule(javafx.scene.shape.FillRule.NON_ZERO);
+                break;
+            default:
+                throw new IllegalArgumentException("illegal winding rule " + iter.getWindingRule());
         }
 
         List<PathElement> fxelem = fxpath.getElements();
         double[] coords = new double[6];
         for (; !iter.isDone(); iter.next()) {
             switch (iter.currentSegment(coords)) {
-            case PathIterator.SEG_CLOSE:
-                fxelem.add(new ClosePath());
-                break;
-            case PathIterator.SEG_CUBICTO:
-                fxelem.add(new CubicCurveTo(coords[0], coords[1], coords[2], coords[3], coords[4], coords[5]));
-                break;
-            case PathIterator.SEG_LINETO:
-                fxelem.add(new LineTo(coords[0], coords[1]));
-                break;
-            case PathIterator.SEG_MOVETO:
-                fxelem.add(new MoveTo(coords[0], coords[1]));
-                break;
-            case PathIterator.SEG_QUADTO:
-                fxelem.add(new QuadCurveTo(coords[0], coords[1], coords[2], coords[3]));
-                break;
+                case PathIterator.SEG_CLOSE:
+                    fxelem.add(new ClosePath());
+                    break;
+                case PathIterator.SEG_CUBICTO:
+                    fxelem.add(new CubicCurveTo(coords[0], coords[1], coords[2], coords[3], coords[4], coords[5]));
+                    break;
+                case PathIterator.SEG_LINETO:
+                    fxelem.add(new LineTo(coords[0], coords[1]));
+                    break;
+                case PathIterator.SEG_MOVETO:
+                    fxelem.add(new MoveTo(coords[0], coords[1]));
+                    break;
+                case PathIterator.SEG_QUADTO:
+                    fxelem.add(new QuadCurveTo(coords[0], coords[1], coords[2], coords[3]));
+                    break;
             }
         }
         return fxpath;
+    }
+
+    /**
+     * Converts a Java Path iterator to a JavaFX shape.
+     *
+     * @param shape A JavaFX shape
+     * @return AWT Shape
+     */
+    public static Shape awtShapeFromFX(javafx.scene.shape.Shape fx) {
+        if (fx instanceof Arc) {
+            return shapeFromFXArc((Arc) fx);
+        } else if (fx instanceof Circle) {
+            return shapeFromFXCircle((Circle) fx);
+        } else if (fx instanceof CubicCurve) {
+            return shapeFromFXCubicCurve((CubicCurve) fx);
+        } else if (fx instanceof Ellipse) {
+            return shapeFromFXEllipse((Ellipse) fx);
+        } else if (fx instanceof Line) {
+            return shapeFromFXLine((Line) fx);
+        } else if (fx instanceof Path) {
+            return shapeFromFXPath((Path) fx);
+        } else if (fx instanceof Polygon) {
+            return shapeFromFXPolygon((Polygon) fx);
+        } else if (fx instanceof Polyline) {
+            return shapeFromFXPolyline((Polyline) fx);
+        } else if (fx instanceof QuadCurve) {
+            return shapeFromFXQuadCurve((QuadCurve) fx);
+        } else if (fx instanceof Rectangle) {
+            return shapeFromFXRectangle((Rectangle) fx);
+        } else if (fx instanceof SVGPath) {
+            return shapeFromFXSVGPath((SVGPath) fx);
+        } else {
+            throw new UnsupportedOperationException("unsupported shape " + fx);
+        }
+    }
+
+    private static Shape shapeFromFXArc(Arc node) {
+        double centerX = node.getCenterX();
+        double centerY = node.getCenterY();
+        double radiusX = node.getRadiusX();
+        double radiusY = node.getRadiusY();
+        double startAngle = Math.toRadians(-node.getStartAngle());
+        double endAngle = Math.toRadians(-node.getStartAngle() - node.getLength());
+        double length = node.getLength();
+
+        double startX = radiusX * Math.cos(startAngle);
+        double startY = radiusY * Math.sin(startAngle);
+
+        double endX = centerX + radiusX * Math.cos(endAngle);
+        double endY = centerY + radiusY * Math.sin(endAngle);
+
+        int xAxisRot = 0;
+        boolean largeArc = (length > 180);
+        boolean sweep = (length < 0);
+
+        BezierPath p = new BezierPath();
+        p.moveTo(centerX, centerY);
+
+        if (ArcType.ROUND == node.getType()) {
+            p.lineTo(startX, startY);
+        }
+
+        p.arcTo(radiusX, radiusY, xAxisRot, largeArc, sweep, endX, endY);
+
+        if (ArcType.CHORD == node.getType()
+                || ArcType.ROUND == node.getType()) {
+            p.setClosed(true);
+        }
+        return p.toGeneralPath();
+    }
+
+    private static Shape shapeFromFXCircle(Circle node) {
+        double x = node.getCenterX();
+        double y = node.getCenterY();
+        double r = node.getRadius();
+        return new Ellipse2D.Double(x - r, y - r, r * 2, r * 2);
+    }
+
+    private static Shape shapeFromFXCubicCurve(CubicCurve e) {
+        Path2D.Double p = new Path2D.Double();
+        p.moveTo(e.getStartX(), e.getStartY());
+        p.curveTo(e.getControlX1(), e.getControlY1(), e.getControlX2(), e.getControlY2(), e.getEndX(), e.getEndY());
+        return p;
+    }
+
+    private static Shape shapeFromFXEllipse(Ellipse node) {
+        double x = node.getCenterX();
+        double y = node.getCenterY();
+        double rx = node.getRadiusX();
+        double ry = node.getRadiusY();
+        return new Ellipse2D.Double(x - rx, y - ry, rx * 2, ry * 2);
+    }
+
+    private static Shape shapeFromFXLine(Line node) {
+        Line2D.Double p = new Line2D.Double(node.getStartX(), node.getStartY(), node.getEndX(), node.getEndY());
+        return p;
+    }
+
+    private static Shape shapeFromFXPath(Path node) {
+        BezierPath p = new BezierPath();
+        double x = 0;
+        double y = 0;
+        for (PathElement pe : node.getElements()) {
+            if (pe instanceof MoveTo) {
+                MoveTo e = (MoveTo) pe;
+                x = e.getX();
+                y = e.getY();
+                p.moveTo(x, y);
+            } else if (pe instanceof LineTo) {
+                LineTo e = (LineTo) pe;
+                x = e.getX();
+                y = e.getY();
+                p.lineTo(x, y);
+            } else if (pe instanceof CubicCurveTo) {
+                CubicCurveTo e = (CubicCurveTo) pe;
+                x = e.getX();
+                y = e.getY();
+                p.curveTo(e.getControlX1(), e.getControlY1(), e.getControlX2(), e.getControlY2(), x, y);
+            } else if (pe instanceof QuadCurveTo) {
+                QuadCurveTo e = (QuadCurveTo) pe;
+                x = e.getX();
+                y = e.getY();
+                p.quadTo(e.getControlX(), e.getControlY(), x, y);
+            } else if (pe instanceof ArcTo) {
+                ArcTo e = (ArcTo) pe;
+                x = e.getX();
+                y = e.getY();
+                p.arcTo(e.getRadiusX(), e.getRadiusY(), e.getXAxisRotation(), e.isLargeArcFlag(), e.isSweepFlag(), x, y);
+            } else if (pe instanceof HLineTo) {
+                HLineTo e = (HLineTo) pe;
+                x = e.getX();
+                p.lineTo(x, y);
+            } else if (pe instanceof VLineTo) {
+                VLineTo e = (VLineTo) pe;
+                y = e.getY();
+                p.lineTo(x, y);
+            } else if (pe instanceof ClosePath) {
+                p.closePath();
+            }
+        }
+        return p.toGeneralPath();
+    }
+
+    private static Shape shapeFromFXPolygon(Polygon node) {
+        Path2D.Double p = new Path2D.Double();
+        List<Double> ps = node.getPoints();
+        for (int i = 0, n = ps.size(); i < n; i += 2) {
+            if (i == 0) {
+                p.moveTo(ps.get(i), ps.get(i + 1));
+            } else {
+                p.lineTo(ps.get(i), ps.get(i + 1));
+            }
+        }
+        p.closePath();
+        return p;
+    }
+
+    private static Shape shapeFromFXPolyline(Polyline node) {
+        Path2D.Double p = new Path2D.Double();
+        List<Double> ps = node.getPoints();
+        for (int i = 0, n = ps.size(); i < n; i += 2) {
+            if (i == 0) {
+                p.moveTo(ps.get(i), ps.get(i + 1));
+            } else {
+                p.lineTo(ps.get(i), ps.get(i + 1));
+            }
+        }
+        return p;
+    }
+
+    private static Shape shapeFromFXQuadCurve(QuadCurve node) {
+        Path2D.Double p = new Path2D.Double();
+        p.moveTo(node.getStartX(), node.getStartY());
+        p.quadTo(node.getControlX(), node.getControlY(), node.getEndX(), node.getEndY());
+        return p;
+    }
+
+    private static Shape shapeFromFXRectangle(Rectangle node) {
+        if (node.getArcHeight() == 0 && node.getArcWidth() == 0) {
+            return new Rectangle2D.Double(
+                    node.getX(),
+                    node.getY(),
+                    node.getWidth(),
+                    node.getHeight()
+            );
+
+        } else {
+            return new RoundRectangle2D.Double(
+                    node.getX(),
+                    node.getY(),
+                    node.getWidth(),
+                    node.getHeight(),
+                    node.getArcWidth(),
+                    node.getArcHeight()
+            );
+        }
+    }
+
+    private static Shape shapeFromFXSVGPath(SVGPath node) {
+       Path p = (Path) javafx.scene.shape.Shape.subtract(node, new Rectangle(0,0));
+       return shapeFromFXPath(p);
     }
 
 }
