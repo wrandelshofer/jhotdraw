592be48d8ebe751da74e28acae41f669b54e2bf4
rawcoder <werner.randelshofer@fibermail.ch>
2017-01-13 21:34:33 +0000

    Improves performance of immutable collections.

--- jhotdraw8/src/main/java/org/jhotdraw8/collection/ImmutableObservableList.java
+++ jhotdraw8/src/main/java/org/jhotdraw8/collection/ImmutableObservableList.java
@@ -6,6 +6,7 @@ package org.jhotdraw8.collection;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableListBase;
 
@@ -15,26 +16,81 @@ import javafx.collections.ObservableListBase;
  * @author Werner Randelshofer
  * @version $$Id: ImmutableObservableList.java 1239 2016-12-20 11:19:20Z
  * rawcoder $$
+ * @param <E> element type
  */
-public final class ImmutableObservableList<T> extends ObservableListBase<T> implements ObservableList<T> {
-
-    private final Object[] array;
+public final class ImmutableObservableList<E> extends ObservableListBase<E> implements ObservableList<E> {
 
     private final static ImmutableObservableList<Object> EMPTY = new ImmutableObservableList<Object>(true, new Object[0]);
 
-    public ImmutableObservableList(Collection<T> copyItems) {
+    private final Object[] array;
+
+    public ImmutableObservableList(Collection<E> copyItems) {
         this.array = copyItems.toArray();
     }
-    private ImmutableObservableList(boolean isPrivate, Object... array) {
+
+    public ImmutableObservableList(Object[] array) {
+        this(array, 0, array.length);
+    }
+
+    public ImmutableObservableList(Object[] a, int offset, int length) {
+        this.array = new Object[length];
+        System.arraycopy(a, offset, array, 0, length);
+    }
+
+    private ImmutableObservableList(boolean privateMethod, Object[] array) {
         this.array = array;
     }
 
+    @Override
     @SafeVarargs
-    public static <T> ImmutableObservableList<T> of(T... items) {
-        return new ImmutableObservableList<>(true, items);
+    public final boolean addAll(E... elements) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E get(int index) {
+        @SuppressWarnings("unchecked")
+        E value = (E) array[index];
+        return value;
+    }
+
+    @Override
+    public void remove(int from, int to) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    @SafeVarargs
+    public final boolean removeAll(E... elements) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    @SafeVarargs
+    public final boolean retainAll(E... elements) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    @SafeVarargs
+    public final boolean setAll(E... elements) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean setAll(Collection<? extends E> col) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int size() {
+        return array.length;
     }
 
     public static <T> ImmutableObservableList<T> add(Collection<T> collection, T item) {
+        if (collection.isEmpty()) {
+            return ImmutableObservableList.of(item);
+        }
         Object[] a = new Object[collection.size() + 1];
         a = collection.toArray(a);
         a[a.length - 1] = item;
@@ -42,6 +98,9 @@ public final class ImmutableObservableList<T> extends ObservableListBase<T> impl
     }
 
     public static <T> ImmutableObservableList<T> add(Collection<T> collection, int index, T item) {
+        if (collection.isEmpty() && index == 0) {
+            return ImmutableObservableList.of(item);
+        }
         Object[] a = new Object[collection.size()];
         a = collection.toArray(a);
         Object[] b = new Object[a.length + 1];
@@ -51,14 +110,26 @@ public final class ImmutableObservableList<T> extends ObservableListBase<T> impl
         return new ImmutableObservableList<>(true, b);
     }
 
-    public static <T> ImmutableObservableList<T> set(Collection<T> collection, int index, T item) {
-        Object[] a = new Object[collection.size()];
-        a = collection.toArray(a);
-        a[index] = item;
-        return new ImmutableObservableList<>(true, a);
+    @SuppressWarnings("unchecked")
+    public static <T> ImmutableObservableList<T> emptyList() {
+        return (ImmutableObservableList<T>) EMPTY;
+    }
+
+    public void copyInto(Object[] out, int offset) {
+        System.arraycopy(array, 0, out, offset, array.length);
+    }
+
+    @SafeVarargs
+    @SuppressWarnings("varargs")
+    public static <T> ImmutableObservableList<T> of(T... items) {
+        // FIXME we should copy the items array, because caller might keep reference on mutable items array
+        return items.length == 0 ? emptyList() : new ImmutableObservableList<>(true, items);
     }
 
     public static <T> ImmutableObservableList<T> remove(Collection<T> collection, int index) {
+        if (collection.size() == 1 && index == 0) {
+            return ImmutableObservableList.emptyList();
+        }
         Object[] a = new Object[collection.size()];
         a = collection.toArray(a);
         Object[] b = new Object[a.length - 1];
@@ -68,65 +139,23 @@ public final class ImmutableObservableList<T> extends ObservableListBase<T> impl
     }
 
     public static <T> ImmutableObservableList<T> remove(Collection<T> collection, T item) {
-        ArrayList<T> a = new ArrayList<T>(collection);
-        a.remove(item);
-        return new ImmutableObservableList<>(a);
+        if (collection.size() == 1 && collection.contains(item)) {
+            return ImmutableObservableList.emptyList();
+        }
+        if (collection instanceof List) {
+            List<T> list = (List) collection;
+            return remove(list, list.indexOf(item));
+        } else {
+            ArrayList<T> a = new ArrayList<T>(collection);// linear
+            a.remove(item);// linear
+            return new ImmutableObservableList<>(a);// linear
+        }
     }
 
-    @SuppressWarnings("unchecked")
-    public static <T> ImmutableObservableList<T> emptyList() {
-        return (ImmutableObservableList<T>) EMPTY;
+    public static <T> ImmutableObservableList<T> set(Collection<T> collection, int index, T item) {
+        Object[] a = new Object[collection.size()];
+        a = collection.toArray(a);
+        a[index] = item;
+        return new ImmutableObservableList<>(true, a);
     }
-
-    @SuppressWarnings("unchecked")
-    public static <T> ImmutableObservableList<T> of() {
-        return emptyList();
-    }
-
-    @Override
-    public T get(int index) {
-        @SuppressWarnings("unchecked")
-        T value = (T) array[index];
-        return value;
-    }
-
-    @Override
-    public int size() {
-        return array.length;
-    }
-
-    @Override
-    @SafeVarargs
-    public final boolean addAll(T... elements) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    @SafeVarargs
-    public final boolean setAll(T... elements) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean setAll(Collection<? extends T> col) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    @SafeVarargs
-    public final boolean removeAll(T... elements) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    @SafeVarargs
-    public final boolean retainAll(T... elements) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void remove(int from, int to) {
-        throw new UnsupportedOperationException();
-    }
-
 }
