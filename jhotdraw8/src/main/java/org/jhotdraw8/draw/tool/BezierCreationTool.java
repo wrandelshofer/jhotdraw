3482ea183e0a146916d348435187984d29f48d5c
rawcoder <werner.randelshofer@fibermail.ch>
2017-01-21 09:53:26 +0000

    Removes Bezier class.

--- jhotdraw8/src/main/java/org/jhotdraw8/draw/tool/BezierCreationTool.java
+++ jhotdraw8/src/main/java/org/jhotdraw8/draw/tool/BezierCreationTool.java
@@ -4,30 +4,26 @@
  */
 package org.jhotdraw8.draw.tool;
 
-import java.util.function.Supplier;
-import javafx.geometry.Point2D;
-import javafx.scene.input.MouseEvent;
-import org.jhotdraw8.draw.DrawingView;
-import org.jhotdraw8.draw.figure.Figure;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Supplier;
+import javafx.geometry.Point2D;
 import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
 import org.jhotdraw8.collection.ImmutableObservableList;
-import org.jhotdraw8.draw.model.DrawingModel;
-import org.jhotdraw8.draw.figure.SimpleLayer;
-import org.jhotdraw8.util.Resources;
-import org.jhotdraw8.draw.figure.Layer;
+import org.jhotdraw8.draw.DrawingView;
 import org.jhotdraw8.draw.SimpleDrawingEditor;
+import org.jhotdraw8.draw.figure.Figure;
+import org.jhotdraw8.draw.figure.Layer;
+import org.jhotdraw8.draw.figure.SimpleLayer;
 import org.jhotdraw8.draw.handle.HandleType;
 import org.jhotdraw8.draw.key.BezierNodeListStyleableFigureKey;
-import org.jhotdraw8.geom.Bezier;
+import org.jhotdraw8.draw.model.DrawingModel;
 import org.jhotdraw8.geom.BezierFit;
 import org.jhotdraw8.geom.BezierNode;
-import org.jhotdraw8.geom.BezierNodePath;
 import org.jhotdraw8.geom.BezierNodePathBuilder;
-import org.jhotdraw8.geom.BezierPath;
-import org.jhotdraw8.geom.Shapes;
+import org.jhotdraw8.util.Resources;
 
 /**
  * CreationTool for bezier figures.
@@ -41,17 +37,16 @@ import org.jhotdraw8.geom.Shapes;
 public class BezierCreationTool extends AbstractCreationTool<Figure> {
 
     /**
-     * The rubber band.
-     */
-    private ArrayList<BezierNode> points;
-
-    /**
      * Remembers the number of points that we had, when the user started to drag
      * the mouse.
      */
     private int dragStartIndex;
 
     private final BezierNodeListStyleableFigureKey key;
+    /**
+     * The rubber band.
+     */
+    private ArrayList<BezierNode> points;
 
     public BezierCreationTool(String name, Resources rsrc, BezierNodeListStyleableFigureKey key, Supplier<Figure> factory) {
         this(name, rsrc, key, factory, SimpleLayer::new);
@@ -63,12 +58,70 @@ public class BezierCreationTool extends AbstractCreationTool<Figure> {
         node.setCursor(Cursor.CROSSHAIR);
     }
 
+    /**
+     * This implementation is empty.
+     */
     @Override
-    protected void stopEditing() {
-        if (createdFigure != null) {
-            createdFigure = null;
-            points = null;
+    public void activate(SimpleDrawingEditor editor) {
+        requestFocus();
+        createdFigure = null;
+    }
+
+    @Override
+    protected void handleMouseClicked(MouseEvent event, DrawingView dv) {
+        if (event.getClickCount() > 1) {
+            if (createdFigure != null) {
+                for (int i = points.size() - 1; i > 0; i--) {
+                    if (Objects.equals(points.get(i), points.get(i - 1))) {
+                        points.remove(i);
+                    }
+                }
+                DrawingModel dm = dv.getModel();
+                if (points.size() < 2) {
+                    dm.removeFromParent(createdFigure);
+                } else {
+                    dm.set(createdFigure, key, new ImmutableObservableList<>(points));
+                    dv.getSelectedFigures().clear();
+                    dv.setHandleType(HandleType.POINT);
+                    dv.getSelectedFigures().add(createdFigure);
+                }
+                createdFigure = null;
+                points = null;
+                fireToolDone();
+            }
         }
+    }
+
+    @Override
+    protected void handleMouseDragged(MouseEvent event, DrawingView dv) {
+        if (createdFigure != null) {
+            double x2 = event.getX();
+            double y2 = event.getY();
+            Point2D c2 = dv.viewToDrawing(x2, y2);
+            DrawingModel dm = dv.getModel();
+            if (dragStartIndex == -1) {
+                points.set(points.size() - 1, new BezierNode(c2));
+                dragStartIndex = points.size() - 2;
+            } else {
+                points.add(new BezierNode(c2));
+            }
+            dm.set(createdFigure, key, new ImmutableObservableList<>(points));
+        }
+        event.consume();
+    }
+
+    @Override
+    protected void handleMouseMoved(MouseEvent event, DrawingView dv) {
+        if (createdFigure != null) {
+            dragStartIndex = -1;
+            double x2 = event.getX();
+            double y2 = event.getY();
+            Point2D c2 = dv.getConstrainer().constrainPoint(createdFigure, dv.viewToDrawing(x2, y2));
+            DrawingModel dm = dv.getModel();
+            points.set(points.size() - 1, new BezierNode(c2));
+            dm.set(createdFigure, key, new ImmutableObservableList<>(points));
+        }
+        event.consume();
     }
 
     @Override
@@ -137,69 +190,11 @@ public class BezierCreationTool extends AbstractCreationTool<Figure> {
     }
 
     @Override
-    protected void handleMouseMoved(MouseEvent event, DrawingView dv) {
+    protected void stopEditing() {
         if (createdFigure != null) {
-            dragStartIndex = -1;
-            double x2 = event.getX();
-            double y2 = event.getY();
-            Point2D c2 = dv.getConstrainer().constrainPoint(createdFigure, dv.viewToDrawing(x2, y2));
-            DrawingModel dm = dv.getModel();
-            points.set(points.size() - 1, new BezierNode(c2));
-            dm.set(createdFigure, key, new ImmutableObservableList<>(points));
+            createdFigure = null;
+            points = null;
         }
-        event.consume();
-    }
-
-    @Override
-    protected void handleMouseDragged(MouseEvent event, DrawingView dv) {
-        if (createdFigure != null) {
-            double x2 = event.getX();
-            double y2 = event.getY();
-            Point2D c2 = dv.viewToDrawing(x2, y2);
-            DrawingModel dm = dv.getModel();
-            if (dragStartIndex == -1) {
-                points.set(points.size() - 1, new BezierNode(c2));
-                dragStartIndex = points.size() - 2;
-            } else {
-                points.add(new BezierNode(c2));
-            }
-            dm.set(createdFigure, key, new ImmutableObservableList<>(points));
-        }
-        event.consume();
-    }
-
-    @Override
-    protected void handleMouseClicked(MouseEvent event, DrawingView dv) {
-        if (event.getClickCount() > 1) {
-            if (createdFigure != null) {
-                for (int i = points.size() - 1; i > 0; i--) {
-                    if (Objects.equals(points.get(i), points.get(i - 1))) {
-                        points.remove(i);
-                    }
-                }
-                DrawingModel dm = dv.getModel();
-                if (points.size() < 2) {
-                    dm.removeFromParent(createdFigure);
-                } else {
-                    dm.set(createdFigure, key, new ImmutableObservableList<>(points));
-                    dv.getSelectedFigures().clear();
-                    dv.setHandleType(HandleType.POINT);
-                    dv.getSelectedFigures().add(createdFigure);
-                }
-                createdFigure = null;
-                points = null;
-                fireToolDone();
-            }
-        }
-    }
-
-    /**
-     * This implementation is empty.
-     */
-    @Override
-    public void activate(SimpleDrawingEditor editor) {
-        requestFocus();
-        createdFigure = null;
     }
 
 }
