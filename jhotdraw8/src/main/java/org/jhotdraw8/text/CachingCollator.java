2e51aeff68d7614324b5b1fafa87bc4c59caa1d4
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2017-04-19 07:06:23 +0200

    Improves sorting performance.

---
+++ jhotdraw8/src/main/java/org/jhotdraw8/text/CachingCollator.java
@@ -0,0 +1,38 @@
+/* @(#)CachingCollator.java
+ * Copyright (c) 2017 by the authors and contributors of JHotDraw.
+ * You may only use this file in compliance with the accompanying license terms.
+ */
+package org.jhotdraw8.text;
+
+import java.text.CollationKey;
+import java.text.Collator;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * CachingCollator.
+ *
+ * @author Werner Randelshofer
+ * @version $$Id$$
+ */
+public class CachingCollator implements Comparator<String> {
+
+    private final Collator collator;
+    private final Map<String, CollationKey> keyMap = new HashMap<>();
+
+    public CachingCollator(Collator collator) {
+        this.collator = collator;
+    }
+
+    @Override
+    public int compare(String o1, String o2) {
+        CollationKey k1 = keyMap.computeIfAbsent(o1==null?"":o1, collator::getCollationKey);
+        CollationKey k2 = keyMap.computeIfAbsent(o2==null?"":o2, collator::getCollationKey);
+        return k1.compareTo(k2);
+    }
+
+    public void clearCache() {
+        keyMap.clear();
+    }
+}
