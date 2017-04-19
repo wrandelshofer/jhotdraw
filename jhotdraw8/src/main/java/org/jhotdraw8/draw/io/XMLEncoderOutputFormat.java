196b64aee20e3e52b1968d29eac68b1b39b3e3e1
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2017-04-19 07:03:52 +0200

    Adds experimental XMLEncoderOutputFormat.

---
+++ jhotdraw8/src/main/java/org/jhotdraw8/draw/io/XMLEncoderOutputFormat.java
@@ -0,0 +1,45 @@
+/* @(#)XMLEncoderOutputFormat.java
+ * Copyright (c) 2017 by the authors and contributors of JHotDraw.
+ * You may only use this file in compliance with the accompanying license terms.
+ */
+package org.jhotdraw8.draw.io;
+
+import java.beans.XMLEncoder;
+import java.io.IOException;
+import java.io.OutputStream;
+import javafx.scene.input.DataFormat;
+import org.jhotdraw8.draw.figure.Drawing;
+import org.jhotdraw8.draw.figure.Figure;
+import org.jhotdraw8.draw.figure.FigurePersistenceDelegate;
+import org.jhotdraw8.draw.figure.RectangleFigure;
+
+/**
+ * XMLEncoderOutputFormat.
+ *
+ * @author Werner Randelshofer
+ * @version $$Id$$
+ */
+public class XMLEncoderOutputFormat implements OutputFormat {
+
+    public final static DataFormat XML_SERIALIZER_FORMAT;
+
+    static {
+        DataFormat fmt = DataFormat.lookupMimeType("application/xml+ser");
+        if (fmt == null) {
+            fmt = new DataFormat("application/xml+ser");
+        }
+        XML_SERIALIZER_FORMAT = fmt;
+    }
+
+    @Override
+    public void write(OutputStream out, Drawing drawing) throws IOException {
+        try (XMLEncoder o = new XMLEncoder(out)) {
+            final FigurePersistenceDelegate delegate = new FigurePersistenceDelegate();
+            o.setPersistenceDelegate(Figure.class, delegate);
+            o.setPersistenceDelegate(Drawing.class, delegate);
+            o.setPersistenceDelegate(RectangleFigure.class, delegate);
+            o.writeObject(drawing);
+        }
+    }
+
+}
