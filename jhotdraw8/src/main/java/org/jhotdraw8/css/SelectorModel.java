49a936219fff73900faaa4f15c1946152460adce
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2017-06-29 21:05:15 +0200

    Reformats code.

--- jhotdraw8/src/main/java/org/jhotdraw8/css/SelectorModel.java
+++ jhotdraw8/src/main/java/org/jhotdraw8/css/SelectorModel.java
@@ -16,47 +16,84 @@ import javafx.css.StyleOrigin;
  * @version $Id$
  */
 public interface SelectorModel<T> {
+
     /**
      * This keyword is used to reset a property.
-     * 
-     *<a href="https://www.w3.org/TR/css3-cascade/#initial">Resetting a Property: the 'initial' keyword.</a>
-     */
-String INITIAL_VALUE_KEYWORD="initial";
-    /**
-     * Returns true if the element has the specified id.
      *
-     * @param element An element of the document
-     * @param id an id
-     * @return true if the element has the id
+     * <a href="https://www.w3.org/TR/css3-cascade/#initial">Resetting a
+     * Property: the 'initial' keyword.</a>
      */
-    boolean hasId(T element, String id);
+    String INITIAL_VALUE_KEYWORD = "initial";
 
     /**
-     * Returns true if the element has the specified type.
-     *
-     * @param element An element of the document
-     * @param type an id
-     * @return true if the element has the id
-     */
-    boolean hasType(T element, String type);
-
-    /**
-     * Returns true if the element has the specified class.
-     *
-     * @param element An element of the document
-     * @param clazz an id
-     * @return true if the element has the id
-     */
-    boolean hasStyleClass(T element, String clazz);
-
-    /**
-     * Returns true if the element has the specified attribute.
+     * Returns true if the element has an attribute with the specified name and
+     * the value contains the specified substring.
      *
      * @param element An element of the document
      * @param attributeName an attribute name
-     * @return true if the element has an attribute with the specified name
+     * @param substring the substring
+     * @return true if the element has an attribute with the specified name and
+     * the value contains the specified substring.
      */
-    boolean hasAttribute(T element, String attributeName);
+    boolean attributeValueContains(T element, String attributeName, String substring);
+
+    /**
+     * Returns true if the element has an attribute with the specified name and
+     * the value is a list of words which contains the specified word.
+     *
+     * @param element An element of the document
+     * @param attributeName an attribute name
+     * @param word the word
+     * @return true if the element has an attribute with the specified name and
+     * the value contains the specified word.
+     */
+    boolean attributeValueContainsWord(T element, String attributeName, String word);
+
+    /**
+     * Returns true if the element has an attribute with the specified name and
+     * the attribute value ends with the specified substring.
+     *
+     * @param element An element of the document
+     * @param attributeName an attribute name
+     * @param substring the substring
+     * @return true if the element has an attribute with the specified name and
+     * the value ends with the specified substring.
+     */
+    boolean attributeValueEndsWith(T element, String attributeName, String substring);
+
+    /**
+     * Returns true if the element has an attribute with the specified name and
+     * value.
+     *
+     * @param element An element of the document
+     * @param attributeName an attribute name
+     * @param attributeValue the attribute value
+     * @return true if the element has an attribute with the specified name and
+     * value
+     */
+    boolean attributeValueEquals(T element, String attributeName, String attributeValue);
+
+    /**
+     * Returns true if the element has an attribute with the specified name and
+     * the attribute value starts with the specified substring.
+     *
+     * @param element An element of the document
+     * @param attributeName an attribute name
+     * @param substring the substring
+     * @return true if the element has an attribute with the specified name and
+     * the value starts with the specified substring.
+     */
+    boolean attributeValueStartsWith(T element, String attributeName, String substring);
+
+    /**
+     * Returns the attribute value with the given name.
+     *
+     * @param element The element
+     * @param name The attribute name
+     * @return The attribute value. Returns null if the element does not have an
+     * attribute with this name.
+     */
+    String getAttribute(T element, String name);
 
     /**
      * Returns all styleable attributes of the element.
@@ -91,73 +128,12 @@ String INITIAL_VALUE_KEYWORD="initial";
     Set<String> getDecomposedAttributeNames(T element);
 
     /**
-     * Returns true if the element has an attribute with the specified name and
-     * value.
+     * Returns the id of the element.
      *
-     * @param element An element of the document
-     * @param attributeName an attribute name
-     * @param attributeValue the attribute value
-     * @return true if the element has an attribute with the specified name and
-     * value
+     * @param element the element
+     * @return the id or null if the element does not have an id.
      */
-    boolean attributeValueEquals(T element, String attributeName, String attributeValue);
-
-    /**
-     * Returns true if the element has an attribute with the specified name and
-     * the attribute value starts with the specified substring.
-     *
-     * @param element An element of the document
-     * @param attributeName an attribute name
-     * @param substring the substring
-     * @return true if the element has an attribute with the specified name and
-     * the value starts with the specified substring.
-     */
-    boolean attributeValueStartsWith(T element, String attributeName, String substring);
-
-    /**
-     * Returns true if the element has an attribute with the specified name and
-     * the attribute value ends with the specified substring.
-     *
-     * @param element An element of the document
-     * @param attributeName an attribute name
-     * @param substring the substring
-     * @return true if the element has an attribute with the specified name and
-     * the value ends with the specified substring.
-     */
-    boolean attributeValueEndsWith(T element, String attributeName, String substring);
-
-    /**
-     * Returns true if the element has an attribute with the specified name and
-     * the value contains the specified substring.
-     *
-     * @param element An element of the document
-     * @param attributeName an attribute name
-     * @param substring the substring
-     * @return true if the element has an attribute with the specified name and
-     * the value contains the specified substring.
-     */
-    boolean attributeValueContains(T element, String attributeName, String substring);
-
-    /**
-     * Returns true if the element has an attribute with the specified name and
-     * the value is a list of words which contains the specified word.
-     *
-     * @param element An element of the document
-     * @param attributeName an attribute name
-     * @param word the word
-     * @return true if the element has an attribute with the specified name and
-     * the value contains the specified word.
-     */
-    boolean attributeValueContainsWord(T element, String attributeName, String word);
-
-    /**
-     * Returns true if the element has the specified pseudo class.
-     *
-     * @param element An element of the document
-     * @param pseudoClass an id
-     * @return true if the element has the id
-     */
-    boolean hasPseudoClass(T element, String pseudoClass);
+    String getId(T element);
 
     /**
      * Gets the parent of the element.
@@ -177,14 +153,65 @@ String INITIAL_VALUE_KEYWORD="initial";
     T getPreviousSibling(T element);
 
     /**
-     * Returns the attribute value with the given name.
+     * Returns the style classes of the element.
      *
-     * @param element The element
-     * @param name The attribute name
-     * @return The attribute value. Returns null if the element does not have an
-     * attribute with this name.
+     * @param element the element
+     * @return the style classes or an empty set.
      */
-    String getAttribute(T element, String name);
+    Set<String> getStyleClasses(T element);
+
+    /**
+     * Returns the style type of the element.
+     *
+     * @param element the element
+     * @return the style type of the element.
+     */
+    String getType(T element);
+
+    /**
+     * Returns true if the element has the specified attribute.
+     *
+     * @param element An element of the document
+     * @param attributeName an attribute name
+     * @return true if the element has an attribute with the specified name
+     */
+    boolean hasAttribute(T element, String attributeName);
+
+    /**
+     * Returns true if the element has the specified id.
+     *
+     * @param element An element of the document
+     * @param id an id
+     * @return true if the element has the id
+     */
+    boolean hasId(T element, String id);
+
+    /**
+     * Returns true if the element has the specified pseudo class.
+     *
+     * @param element An element of the document
+     * @param pseudoClass an id
+     * @return true if the element has the id
+     */
+    boolean hasPseudoClass(T element, String pseudoClass);
+
+    /**
+     * Returns true if the element has the specified class.
+     *
+     * @param element An element of the document
+     * @param clazz an id
+     * @return true if the element has the id
+     */
+    boolean hasStyleClass(T element, String clazz);
+
+    /**
+     * Returns true if the element has the specified type.
+     *
+     * @param element An element of the document
+     * @param type an id
+     * @return true if the element has the id
+     */
+    boolean hasType(T element, String type);
 
     /**
      * Sets an attribute value.
@@ -196,28 +223,4 @@ String INITIAL_VALUE_KEYWORD="initial";
      * element.
      */
     void setAttribute(T element, StyleOrigin origin, String name, String value);
-
-    /**
-     * Returns the id of the element.
-     *
-     * @param element the element
-     * @return the id or null if the element does not have an id.
-     */
-    String getId(T element);
-
-    /**
-     * Returns the style type of the element.
-     *
-     * @param element the element
-     * @return the style type of the element.
-     */
-    String getType(T element);
-
-    /**
-     * Returns the style classes of the element.
-     *
-     * @param element the element
-     * @return the style classes or an empty set.
-     */
-    Set<String> getStyleClasses(T element);
 }
