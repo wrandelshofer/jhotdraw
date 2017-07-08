1a072f0fae287ac11ac92c47515c146904aeebe3
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2017-07-08 16:06:11 +0200

    Now can take PathElements as constructor argument.

--- jhotdraw8/src/main/java/org/jhotdraw8/geom/FXPathBuilder.java
+++ jhotdraw8/src/main/java/org/jhotdraw8/geom/FXPathBuilder.java
@@ -4,12 +4,15 @@
  */
 package org.jhotdraw8.geom;
 
+import java.util.ArrayList;
+import java.util.List;
 import javafx.scene.shape.ArcTo;
 import javafx.scene.shape.ClosePath;
 import javafx.scene.shape.CubicCurveTo;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
+import javafx.scene.shape.PathElement;
 import javafx.scene.shape.QuadCurveTo;
 
 /**
@@ -20,39 +23,50 @@ import javafx.scene.shape.QuadCurveTo;
  */
 public class FXPathBuilder extends AbstractPathBuilder {
 
-    @Override
-    protected void doArcTo(double rx, double ry, double xAxisRotation, double x, double y, boolean largeArcFlag, boolean sweepFlag) {
-        path.getElements().add(new ArcTo(rx, ry, xAxisRotation, x, y, largeArcFlag, sweepFlag));
+    public FXPathBuilder() {
+        this(new ArrayList<PathElement>());
     }
 
-    private Path path = new Path();
+    public FXPathBuilder(List<PathElement> elements) {
+        this.elements = elements;
+    }
+
+    @Override
+    protected void doArcTo(double rx, double ry, double xAxisRotation, double x, double y, boolean largeArcFlag, boolean sweepFlag) {
+        elements.add(new ArcTo(rx, ry, xAxisRotation, x, y, largeArcFlag, sweepFlag));
+    }
+
+    private List<PathElement> elements;
 
     @Override
     protected void doClosePath() {
-        path.getElements().add(new ClosePath());
+        elements.add(new ClosePath());
     }
 
     @Override
     protected void doCurveTo(double x, double y, double x0, double y0, double x1, double y1) {
-        path.getElements().add(new CubicCurveTo(x, y, x0, y0, x1, y1));
+        elements.add(new CubicCurveTo(x, y, x0, y0, x1, y1));
     }
 
     @Override
     protected void doLineTo(double x, double y) {
-        path.getElements().add(new LineTo(x, y));
+        elements.add(new LineTo(x, y));
     }
 
     @Override
     protected void doMoveTo(double x, double y) {
-        path.getElements().add(new MoveTo(x, y));
+        elements.add(new MoveTo(x, y));
     }
 
     @Override
     protected void doQuadTo(double x, double y, double x0, double y0) {
-        path.getElements().add(new QuadCurveTo(x, y, x0, y0));
+        elements.add(new QuadCurveTo(x, y, x0, y0));
     }
 
     public Path get() {
-        return path;
+        return new Path(elements);
+    }
+    public List<PathElement> getElements() {
+        return elements;
     }
 }
