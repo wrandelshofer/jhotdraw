efc5320b2717f69628312857b543efec8339869b
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2017-07-18 06:58:36 +0200

    Adds combined accessors.

--- jhotdraw8/src/main/java/org/jhotdraw8/draw/figure/AbstractLineConnectionWithMarkersFigure.java
+++ jhotdraw8/src/main/java/org/jhotdraw8/draw/figure/AbstractLineConnectionWithMarkersFigure.java
@@ -9,16 +9,15 @@ import java.awt.geom.PathIterator;
 import javafx.css.StyleOrigin;
 import javafx.geometry.Point2D;
 import javafx.scene.Node;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.SVGPath;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Scale;
 import javafx.scene.transform.Translate;
-import org.jhotdraw8.draw.key.CssColor;
 import org.jhotdraw8.draw.key.DirtyBits;
 import org.jhotdraw8.draw.key.DirtyMask;
 import org.jhotdraw8.draw.key.DoubleStyleableFigureKey;
+import org.jhotdraw8.draw.key.Scale2DStyleableMapAccessor;
 import org.jhotdraw8.draw.key.SvgPathStyleableFigureKey;
 import org.jhotdraw8.draw.render.RenderContext;
 import org.jhotdraw8.geom.Shapes;
@@ -34,12 +33,23 @@ public abstract class AbstractLineConnectionWithMarkersFigure extends AbstractLi
         implements PathIterableFigure {
 
     // FIXME should not make this user-editable in this base class!
-    public final static SvgPathStyleableFigureKey START_MARKER = new SvgPathStyleableFigureKey("marker-shape-start", DirtyMask.of(DirtyBits.NODE), null);
-    public final static SvgPathStyleableFigureKey END_MARKER = new SvgPathStyleableFigureKey("marker-shape-end", DirtyMask.of(DirtyBits.NODE), null);
-    public final static DoubleStyleableFigureKey START_MARKER_LINE_INSET = new DoubleStyleableFigureKey("marker-line-inset-start", DirtyMask.of(DirtyBits.NODE), 0.0);
-    public final static DoubleStyleableFigureKey END_MARKER_LINE_INSET = new DoubleStyleableFigureKey("marker-line-inset-end", DirtyMask.of(DirtyBits.NODE), 0.0);
-    public final static DoubleStyleableFigureKey START_MARKER_SCALE_FACTOR = new DoubleStyleableFigureKey("marker-scale-factor-start", DirtyMask.of(DirtyBits.NODE), 1.0);
-    public final static DoubleStyleableFigureKey END_MARKER_SCALE_FACTOR = new DoubleStyleableFigureKey("marker-scale-factor-end", DirtyMask.of(DirtyBits.NODE), 1.0);
+    public final static SvgPathStyleableFigureKey MARKER_START_SHAPE = new SvgPathStyleableFigureKey("marker-shape-start", DirtyMask.of(DirtyBits.NODE), null);
+    public final static SvgPathStyleableFigureKey MARKER_END_SHAPE = new SvgPathStyleableFigureKey("marker-shape-end", DirtyMask.of(DirtyBits.NODE), null);
+    //public final static SvgPathStyleableFigureKey MARKER_MIDDLE_SHAPE = new SvgPathStyleableFigureKey("marker-shape-mid", DirtyMask.of(DirtyBits.NODE), null);
+    public final static DoubleStyleableFigureKey MARKER_LINE_INSET_START = new DoubleStyleableFigureKey("marker-line-inset-start", DirtyMask.of(DirtyBits.NODE), 0.0);
+    public final static DoubleStyleableFigureKey MARKER_LINE_INSET_END = new DoubleStyleableFigureKey("marker-line-inset-end", DirtyMask.of(DirtyBits.NODE), 0.0);
+    public final static DoubleStyleableFigureKey MARKER_SCALE_FACTOR_START = new DoubleStyleableFigureKey("marker-scale-factor-start", DirtyMask.of(DirtyBits.NODE), 1.0);
+    public final static DoubleStyleableFigureKey MARKER_SCALE_FACTOR_END = new DoubleStyleableFigureKey("marker-scale-factor-end", DirtyMask.of(DirtyBits.NODE), 1.0);
+    //public final static DoubleStyleableFigureKey MARKER_SCALE_FACTOR_MIDDLE = new DoubleStyleableFigureKey("marker-scale-factor-mid", DirtyMask.of(DirtyBits.NODE), 1.0);
+    /**
+     * Defines the line insets for start and end marker..
+     */
+    public static Scale2DStyleableMapAccessor MARKER_LINE_INSET = new Scale2DStyleableMapAccessor("marker-line-inset", MARKER_LINE_INSET_START, MARKER_LINE_INSET_END);
+    /**
+     * Defines the scale factor for start and end marker..
+     */
+    public static Scale2DStyleableMapAccessor MARKER_SCALE_FACTOR = new Scale2DStyleableMapAccessor("marker-scale-factor", MARKER_SCALE_FACTOR_START, MARKER_SCALE_FACTOR_END);
+    //public static Scale3DStyleableMapAccessor MARKER_SCALE_FACTOR = new Scale3DStyleableMapAccessor("marker-scale-factor", MARKER_SCALE_FACTOR_START, MARKER_SCALE_FACTOR_END, MARKER_SCALE_FACTOR_MIDDLE);
 
     public AbstractLineConnectionWithMarkersFigure() {
         this(0, 0, 1, 1);
@@ -51,10 +61,10 @@ public abstract class AbstractLineConnectionWithMarkersFigure extends AbstractLi
 
     public AbstractLineConnectionWithMarkersFigure(double startX, double startY, double endX, double endY) {
         super(startX, startY, endX, endY);
-        setStyled(StyleOrigin.USER_AGENT, START_MARKER_LINE_INSET, 10.0);
-        setStyled(StyleOrigin.USER_AGENT, END_MARKER_LINE_INSET, 10.0);
-        setStyled(StyleOrigin.USER_AGENT, START_MARKER, "M0,0 L-10,5 -10,-5Z");
-        setStyled(StyleOrigin.USER_AGENT, END_MARKER, "M0,0 L-10,5 -10,-5Z");
+        setStyled(StyleOrigin.USER_AGENT, MARKER_LINE_INSET_START, 10.0);
+        setStyled(StyleOrigin.USER_AGENT, MARKER_LINE_INSET_END, 10.0);
+        setStyled(StyleOrigin.USER_AGENT, MARKER_START_SHAPE, "M0,0 L-10,5 -10,-5Z");
+        setStyled(StyleOrigin.USER_AGENT, MARKER_END_SHAPE, "M0,0 L-10,5 -10,-5Z");
     }
 
     @Override
@@ -101,20 +111,20 @@ public abstract class AbstractLineConnectionWithMarkersFigure extends AbstractLi
         Point2D start = get(START);
         Point2D end = get(END);
 
-        final double startInset = getStyled(START_MARKER_LINE_INSET);
-        final double endInset = getStyled(END_MARKER_LINE_INSET);
-        final String startMarkerStr = getStyled(START_MARKER);
+        final double startInset = getStyled(MARKER_LINE_INSET_START);
+        final double endInset = getStyled(MARKER_LINE_INSET_END);
+        final String startMarkerStr = getStyled(MARKER_START_SHAPE);
 
-        updateMarkerNode(ctx, g, startMarkerNode, start, end, startMarkerStr, getStyled(START_MARKER_SCALE_FACTOR));
-        final String endMarkerStr = getStyled(END_MARKER);
-        updateMarkerNode(ctx, g, endMarkerNode, end, start, endMarkerStr, getStyled(END_MARKER_SCALE_FACTOR));
+        updateMarkerNode(ctx, g, startMarkerNode, start, end, startMarkerStr, getStyled(MARKER_SCALE_FACTOR_START));
+        final String endMarkerStr = getStyled(MARKER_END_SHAPE);
+        updateMarkerNode(ctx, g, endMarkerNode, end, start, endMarkerStr, getStyled(MARKER_SCALE_FACTOR_END));
 
         Point2D dir = end.subtract(start).normalize();
         if (startInset != 0 && startMarkerStr != null) {
-            start = start.add(dir.multiply(startInset * getStyled(START_MARKER_SCALE_FACTOR)));
+            start = start.add(dir.multiply(startInset * getStyled(MARKER_SCALE_FACTOR_START)));
         }
         if (endInset != 0 && endMarkerStr != null) {
-            end = end.add(dir.multiply(-endInset * getStyled(END_MARKER_SCALE_FACTOR)));
+            end = end.add(dir.multiply(-endInset * getStyled(MARKER_SCALE_FACTOR_END)));
         }
         lineNode.setStartX(start.getX());
         lineNode.setStartY(start.getY());
