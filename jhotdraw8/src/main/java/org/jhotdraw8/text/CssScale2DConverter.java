df8f2272b11f90ea5a6996d8823bab777fbdc870
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2017-07-18 06:58:49 +0200

    Adds classes.

---
+++ jhotdraw8/src/main/java/org/jhotdraw8/text/CssScale2DConverter.java
@@ -0,0 +1,51 @@
+/* @(#)CssScale2DConverter.java
+ * Copyright (c) 2015 by the authors and contributors of JHotDraw.
+ * You may only use this file in compliance with the accompanying license terms.
+ */
+package org.jhotdraw8.text;
+
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.text.ParseException;
+import javafx.geometry.Point2D;
+import org.jhotdraw8.io.IdFactory;
+
+/**
+ * Converts a {@code javafx.geometry.Point2D} into a {@code String} and vice
+ * versa.
+ *
+ * @author Werner Randelshofer
+ * @version $Id$
+ */
+public class CssScale2DConverter implements Converter<Point2D> {
+
+    // FIXME must use CssParser instead of PatternConverter!!
+    private final PatternConverter formatter = new PatternConverter("{0,list,{1,number}|[ ]+}", new CssConverterFactory());
+
+    @Override
+    public void toString(Appendable out, IdFactory idFactory, Point2D value) throws IOException {
+            if (value.getX() == value.getY()) {
+                formatter.toStr(out, idFactory, 1, value.getX());
+            } else {
+                formatter.toStr(out, idFactory, 2, value.getX(), value.getY());
+            }
+    }
+
+    @Override
+    public Point2D fromString(CharBuffer buf, IdFactory idFactory) throws ParseException, IOException {
+        Object[] v = formatter.fromString(buf);
+        switch ((int) v[0]) {
+            case 1:
+                return new Point2D(((Number) v[1]).doubleValue(), ((Number) v[1]).doubleValue());
+            case 2:
+                return new Point2D(((Number) v[1]).doubleValue(), ((Number) v[2]).doubleValue());
+            default:
+                throw new ParseException("Scale with 1 to 2 values expected.", buf.position());
+        }
+    }
+
+    @Override
+    public Point2D getDefaultValue() {
+        return new Point2D(1, 1);
+    }
+}
