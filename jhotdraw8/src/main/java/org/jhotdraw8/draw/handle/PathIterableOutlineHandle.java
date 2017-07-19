ee7d87aa722d13e0a561d4462d551ef096b3522d
Werner Randelshofer <w.randelshofer@systransis.ch>
2017-07-19 17:34:20 +0200

    Adds classes.

---
+++ jhotdraw8/src/main/java/org/jhotdraw8/draw/handle/PathIterableOutlineHandle.java
@@ -0,0 +1,88 @@
+/* @(#)PathIterableOutlineHandle.java
+ * Copyright (c) 2017 by the authors and contributors of JHotDraw.
+ * You may only use this file in compliance with the accompanying license terms.
+ */
+package org.jhotdraw8.draw.handle;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.PathElement;
+import javafx.scene.transform.Transform;
+import org.jhotdraw8.draw.DrawingView;
+import org.jhotdraw8.draw.figure.PathIterableFigure;
+import static org.jhotdraw8.draw.handle.Handle.STYLECLASS_HANDLE;
+import static org.jhotdraw8.draw.handle.Handle.STYLECLASS_HANDLE_MOVE_OUTLINE;
+import org.jhotdraw8.geom.FXPathBuilder;
+import org.jhotdraw8.geom.Shapes;
+import org.jhotdraw8.geom.Transforms;
+
+/**
+ * Draws an outline of the path of a {@link PathIterableFigure}.
+ *
+ * @author Werner Randelshofer
+ * @version $$Id$$
+ */
+public class PathIterableOutlineHandle extends AbstractHandle {
+
+    private final Path node;
+    private final String styleclass;
+    private final boolean selectable;
+
+    public PathIterableOutlineHandle(PathIterableFigure figure, boolean selectable) {
+        this(figure,selectable, STYLECLASS_HANDLE_MOVE_OUTLINE);
+    }
+
+    public PathIterableOutlineHandle(PathIterableFigure figure, boolean selectable, String styleclass) {
+        super(figure);
+        node = new Path();
+        this.styleclass = styleclass;
+        initNode(node);
+        this.selectable=selectable;
+    }
+
+    @Override
+    public boolean contains(DrawingView dv, double x, double y, double tolerance) {
+        return node.contains(x,y);
+    }
+
+    @Override
+    public Cursor getCursor() {
+        return null;
+    }
+
+    @Override
+    public Node getNode() {
+        return node;
+    }
+
+    protected void initNode(Path r) {
+        r.setFill(null);
+        r.setStroke(Color.BLUE);
+        r.getStyleClass().addAll(styleclass, STYLECLASS_HANDLE);
+    }
+
+    @Override
+    public boolean isSelectable() {
+        return selectable;
+    }
+
+    @Override
+    public PathIterableFigure getOwner() {
+        return (PathIterableFigure) super.getOwner();
+    }
+
+    @Override
+    public void updateNode(DrawingView view) {
+        PathIterableFigure f = getOwner();
+        Transform t = Transforms.concat(view.getWorldToView(), f.getLocalToWorld());
+        List<PathElement> elements = new ArrayList<>();
+        FXPathBuilder builder = new FXPathBuilder(elements);
+        Shapes.buildFromPathIterator(builder, f.getPathIterator(Shapes.awtTransformFromFX(t)));
+        node.getElements().setAll(elements);
+    }
+
+}
---
+++ jhotdraw8/src/main/java/org/jhotdraw8/draw/handle/PathIterablePointsHandle.java
@@ -0,0 +1,90 @@
+/* @(#)PathIterableOutlineHandle.java
+ * Copyright (c) 2017 by the authors and contributors of JHotDraw.
+ * You may only use this file in compliance with the accompanying license terms.
+ */
+package org.jhotdraw8.draw.handle;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.PathElement;
+import javafx.scene.transform.Transform;
+import org.jhotdraw8.draw.DrawingView;
+import org.jhotdraw8.draw.figure.PathIterableFigure;
+import static org.jhotdraw8.draw.handle.Handle.STYLECLASS_HANDLE;
+import org.jhotdraw8.geom.FXPathPointsBuilder;
+import org.jhotdraw8.geom.Shapes;
+import org.jhotdraw8.geom.Transforms;
+
+/**
+ * Draws points of the path of a {@PathIterableFigure}.
+ * <p>
+ * Does not provide interactions. Just increases the hit area of the figure.
+ *
+ * @author Werner Randelshofer
+ * @version $$Id$$
+ */
+public class PathIterablePointsHandle extends AbstractHandle {
+
+    private final Path node;
+    private final String styleclass;
+    private final boolean selectable;
+
+    public PathIterablePointsHandle(PathIterableFigure figure, boolean selectable) {
+        this(figure, selectable, STYLECLASS_HANDLE_MOVE);
+    }
+
+    public PathIterablePointsHandle(PathIterableFigure figure, boolean selectable, String styleclass) {
+        super(figure);
+        node = new Path();
+        this.styleclass = styleclass;
+        initNode(node);
+        this.selectable=selectable;
+    }
+
+    @Override
+    public boolean contains(DrawingView dv, double x, double y, double tolerance) {
+        return node.contains(x,y);
+    }
+
+    @Override
+    public Cursor getCursor() {
+        return null;
+    }
+
+    @Override
+    public Node getNode() {
+        return node;
+    }
+
+    protected void initNode(Path r) {
+        r.setFill(null);
+        r.setStroke(Color.BLUE);
+        r.getStyleClass().addAll(styleclass, STYLECLASS_HANDLE);
+    }
+
+    @Override
+    public boolean isSelectable() {
+        return selectable;
+    }
+
+    @Override
+    public PathIterableFigure getOwner() {
+        return (PathIterableFigure) super.getOwner();
+    }
+
+    @Override
+    public void updateNode(DrawingView view) {
+        PathIterableFigure f = getOwner();
+        Transform t = Transforms.concat(view.getWorldToView(), f.getLocalToWorld());
+        List<PathElement> elements = new ArrayList<>();
+        FXPathPointsBuilder builder = new FXPathPointsBuilder(elements);
+        Shapes.buildFromPathIterator(builder, f.getPathIterator(Shapes.awtTransformFromFX(t)));
+        
+        node.getElements().setAll(builder.getElements());
+    }
+
+}
