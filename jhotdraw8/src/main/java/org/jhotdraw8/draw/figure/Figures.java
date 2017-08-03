a6ace4ceef57fe7b74e4d6e6fc962f9cd8bbeda2
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2017-08-03 17:54:56 +0200

    Adds getCenterBounds method.

--- jhotdraw8/src/main/java/org/jhotdraw8/draw/figure/Figures.java
+++ jhotdraw8/src/main/java/org/jhotdraw8/draw/figure/Figures.java
@@ -34,4 +34,19 @@ public class Figures {
         }
         return new BoundingBox(minx, miny, maxx - minx, maxy - miny);
     }
+
+    public static Bounds getCenterBounds(Iterable<? extends Figure> figures) {
+        double minx = Double.MAX_VALUE, miny = Double.MAX_VALUE,
+                maxx = Double.MIN_VALUE, maxy = Double.MIN_VALUE;
+        for (Figure f : figures) {
+            Bounds b = f.localToWorld(f.getBoundsInLocal());
+            double cx = b.getMinX() + b.getWidth() * 0.5;
+            double cy = b.getMinY() + b.getHeight() * 0.5;
+            minx = min(minx, cx);
+            maxx = max(maxx, cx);
+            miny = min(miny, cy);
+            maxy = max(maxy, cy);
+        }
+        return new BoundingBox(minx, miny, maxx - minx, maxy - miny);
+    }
 }
