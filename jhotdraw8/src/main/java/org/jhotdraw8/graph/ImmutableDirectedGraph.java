119e8c94fd29522a937f187e95de79e2ed004b49
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2017-09-20 06:58:22 +0200

    Cleans graph package up.

--- jhotdraw8/src/main/java/org/jhotdraw8/graph/ImmutableDirectedGraph.java
+++ jhotdraw8/src/main/java/org/jhotdraw8/graph/ImmutableDirectedGraph.java
@@ -11,6 +11,7 @@ import java.util.Map;
  *
  * @author Werner Randelshofer
  * @version $Id$
+ * @param <V> the vertex type
  */
 public class ImmutableDirectedGraph<V> implements DirectedGraph<V> {
 
