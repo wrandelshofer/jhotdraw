c3b42d58cb4d5626993f3cd60429e7d4cd34f842
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2017-11-13 19:37:07 +0100

    Renames method.

--- jhotdraw8/src/main/java/org/jhotdraw8/graph/UnorderedPair.java
+++ jhotdraw8/src/main/java/org/jhotdraw8/graph/UnorderedPair.java
@@ -33,8 +33,8 @@ public class UnorderedPair<V> implements Pair<V> {
         return b;
     }
 
-    public boolean isIntersectionEmpty() {
-        return !Objects.equals(a, b);
+    public boolean isStartEqualsEnd() {
+        return Objects.equals(a, b);
     }
 
     @Override
