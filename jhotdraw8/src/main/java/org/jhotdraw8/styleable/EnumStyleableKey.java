b5eec9e74f2048bb0bbb21a3aa16c84ff1e8803f
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2018-01-06 17:37:21 +0100

    Adds class.

---
+++ jhotdraw8/src/main/java/org/jhotdraw8/styleable/EnumStyleableKey.java
@@ -0,0 +1,84 @@
+/* @(#)EnumStyleableFigureKey.java
+ * Copyright © 2017 by the authors and contributors of JHotDraw. MIT License.
+ */
+package org.jhotdraw8.styleable;
+
+import org.jhotdraw8.draw.key.*;
+import javafx.css.CssMetaData;
+import javafx.css.Styleable;
+import javafx.css.StyleablePropertyFactory;
+import org.jhotdraw8.styleable.StyleablePropertyBean;
+import org.jhotdraw8.draw.figure.Figure;
+import org.jhotdraw8.text.Converter;
+import org.jhotdraw8.text.CssEnumConverter;
+import org.jhotdraw8.styleable.WriteableStyleableMapAccessor;
+
+/**
+ * EnumStyleableFigureKey.
+ *
+ * @author Werner Randelshofer
+ * @version $Id$
+ */
+public class EnumStyleableKey<T extends Enum<T>> extends SimpleStyleableKey<T> implements WriteableStyleableMapAccessor<T> {
+
+    private final static long serialVersionUID = 1L;
+
+    private final CssMetaData<?, T> cssMetaData;
+
+    private final boolean nullable;
+
+    /**
+     * Creates a new instance with the specified name, enum class, mask and with
+     * null as the default value.
+     *
+     * @param name The name of the key.
+     * @param clazz The enum class.
+     * @param mask The mask.
+     */
+    public EnumStyleableKey(String name, Class<T> clazz) {
+        this(name, clazz, true, null);
+    }
+
+    /**
+     * Creates a new instance with the specified name, enum class, mask and
+     * default value.
+     *
+     * @param name The name of the key.
+     * @param clazz The enum class.
+     * @param mask The mask.
+     * @param nullable Whether the value is nullable
+     * @param defaultValue The default value.
+     */
+    public EnumStyleableKey(String name, Class<T> clazz, boolean nullable, T defaultValue) {
+        super(name, clazz,null,null, defaultValue);
+
+        this.nullable = nullable;
+
+        if (!nullable && defaultValue == null) {
+            throw new IllegalArgumentException("defaultValue may only be null if nullable=true");
+        }
+
+        StyleablePropertyFactory<?> factory = new StyleablePropertyFactory<Styleable>(null);
+        cssMetaData = factory.createEnumCssMetaData(clazz,
+                Figure.JHOTDRAW_CSS_PREFIX + getName(), s -> {
+                    StyleablePropertyBean spb = (StyleablePropertyBean) s;
+                    return spb.getStyleableProperty(this);
+                });
+    }
+
+    @Override
+    public CssMetaData<?, T> getCssMetaData() {
+        return cssMetaData;
+
+    }
+
+    private Converter<T> converter;
+
+    @Override
+    public Converter<T> getConverter() {
+        if (converter == null) {
+            converter = new CssEnumConverter<>(getValueType(), nullable);
+        }
+        return converter;
+    }
+}
