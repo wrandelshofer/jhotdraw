608736c1e1e8e492e512cb749de70d05f9d18c64
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2018-10-05 17:15:25 +0200

    Minor fixes.

---
+++ jhotdraw8/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module jhotdraw8 {
+    requires javafx.controls;
+    requires java.desktop;
+    requires java.prefs;
+    requires java.logging;
+    requires javafx.fxml;
+    requires javafx.swing;
+    requires javafx.graphics;
+
+}
