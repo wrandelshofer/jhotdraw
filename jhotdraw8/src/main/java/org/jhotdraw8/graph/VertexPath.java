32b2a65b2f629bb3dc7bc63df30ebdd9bf025fae
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2018-12-19 12:38:24 +0100

    Adds size method.

--- jhotdraw8/src/main/java/org/jhotdraw8/graph/VertexPath.java
+++ jhotdraw8/src/main/java/org/jhotdraw8/graph/VertexPath.java
@@ -64,6 +64,10 @@ public class VertexPath<V> {
         return hash;
     }
 
+    public int size() {
+        return vertices.size();
+    }
+
     @Nonnull
     @Override
     public String toString() {
