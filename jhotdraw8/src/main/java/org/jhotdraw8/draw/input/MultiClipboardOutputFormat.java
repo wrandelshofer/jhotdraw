e13e8a3561c51f9935d4f5fac7ecdcdd9ced7b09
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2019-01-05 11:35:14 +0100

    Formats code.

--- jhotdraw8/src/main/java/org/jhotdraw8/draw/input/MultiClipboardOutputFormat.java
+++ jhotdraw8/src/main/java/org/jhotdraw8/draw/input/MultiClipboardOutputFormat.java
@@ -7,7 +7,9 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
 import java.util.function.Supplier;
+
 import javafx.scene.input.DataFormat;
+import org.jhotdraw8.annotation.Nonnull;
 import org.jhotdraw8.annotation.Nullable;
 import org.jhotdraw8.draw.figure.Drawing;
 import org.jhotdraw8.draw.figure.Figure;
@@ -24,29 +26,30 @@ public class MultiClipboardOutputFormat implements ClipboardOutputFormat {
 
     private ClipboardOutputFormat[] formats;
 
-    @SuppressWarnings("unchecked")
-    public MultiClipboardOutputFormat(Supplier<ClipboardOutputFormat>... formatSuppliers) {
+    @SafeVarargs
+    public MultiClipboardOutputFormat(@Nonnull Supplier<ClipboardOutputFormat>... formatSuppliers) {
         this.formatSuppliers = formatSuppliers;
     }
+
     public MultiClipboardOutputFormat(ClipboardOutputFormat... formats) {
         this.formats = formats;
     }
 
     private void createFormats() {
-        if (formatSuppliers!=null) {
-            int n=formatSuppliers.length;
-            formats=new ClipboardOutputFormat[n];
-            for (int i=0;i<n;i++){
-                formats[i]=formatSuppliers[i].get();
+        if (formatSuppliers != null) {
+            int n = formatSuppliers.length;
+            formats = new ClipboardOutputFormat[n];
+            for (int i = 0; i < n; i++) {
+                formats[i] = formatSuppliers[i].get();
             }
-            formatSuppliers=null;
+            formatSuppliers = null;
         }
     }
 
     @Override
     public void write(Map<DataFormat, Object> out, Drawing drawing, Collection<Figure> selection) throws IOException {
         createFormats();
-        
+
         for (ClipboardOutputFormat f : formats) {
             f.write(out, drawing, selection);
         }
