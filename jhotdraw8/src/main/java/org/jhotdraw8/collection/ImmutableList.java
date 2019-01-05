134f91cf3d5a13c8485bb32537dc3c617049acfa
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2019-01-05 11:35:54 +0100

    Fixes compiler warnings. Always copies input arrays.

--- jhotdraw8/src/main/java/org/jhotdraw8/collection/ImmutableList.java
+++ jhotdraw8/src/main/java/org/jhotdraw8/collection/ImmutableList.java
@@ -5,6 +5,7 @@ package org.jhotdraw8.collection;
 
 import org.jhotdraw8.annotation.Nonnull;
 import org.jhotdraw8.annotation.Nullable;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -17,7 +18,7 @@ import java.util.List;
 public interface ImmutableList<E> extends ReadOnlyList<E>, ImmutableCollection<E> {
 
     @Nonnull
-    public static <T> ImmutableList<T> add(@Nullable ReadOnlyCollection<T> collection, T item) {
+    static <T> ImmutableList<T> add(@Nullable ReadOnlyCollection<T> collection, T item) {
         if (collection == null || collection.isEmpty()) {
             return ImmutableList.of(item);
         }
@@ -28,7 +29,7 @@ public interface ImmutableList<E> extends ReadOnlyList<E>, ImmutableCollection<E
     }
 
     @Nonnull
-    public static <T> ImmutableList<T> add(@Nullable Collection<T> collection, T item) {
+    static <T> ImmutableList<T> add(@Nullable Collection<T> collection, T item) {
         if (collection == null || collection.isEmpty()) {
             return ImmutableList.of(item);
         }
@@ -39,7 +40,7 @@ public interface ImmutableList<E> extends ReadOnlyList<E>, ImmutableCollection<E
     }
 
     @Nonnull
-    public static <T> ImmutableList<T> add(@Nullable Collection<T> collection, int index, T item) {
+    static <T> ImmutableList<T> add(@Nullable Collection<T> collection, int index, T item) {
         if (collection == null || collection.isEmpty() && index == 0) {
             return ImmutableList.of(item);
         }
@@ -53,7 +54,7 @@ public interface ImmutableList<E> extends ReadOnlyList<E>, ImmutableCollection<E
     }
 
     @Nonnull
-    public static <T> ImmutableList<T> add(@Nullable ReadOnlyCollection<T> collection, int index, T item) {
+    static <T> ImmutableList<T> add(@Nullable ReadOnlyCollection<T> collection, int index, T item) {
         if (collection == null || collection.isEmpty() && index == 0) {
             return ImmutableList.of(item);
         }
@@ -68,39 +69,39 @@ public interface ImmutableList<E> extends ReadOnlyList<E>, ImmutableCollection<E
 
     @Nonnull
     @SuppressWarnings("unchecked")
-    public static <T> ImmutableList<T> emptyList() {
+    static <T> ImmutableList<T> emptyList() {
         return (ImmutableArrayList<T>) ImmutableArrayList.EMPTY;
     }
 
     @Nonnull
     @SafeVarargs
     @SuppressWarnings("varargs")
-    public static <T> ImmutableList<T> of(T... items) {
-        // FIXME we should copy the items array, because caller might keep reference on mutable items array
-        return items.length == 0 ? emptyList() : new ImmutableArrayList<>(true, items);
+    static <T> ImmutableList<T> of(T... items) {
+        return items.length == 0 ? emptyList() : new ImmutableArrayList<>(items, 0, items.length);
     }
 
     @Nonnull
-    public static <T> ImmutableList<T> ofCollection(Collection<? extends T> collection) {
-        return collection.isEmpty() ? emptyList() : new ImmutableArrayList<T>(collection);
+    static <T> ImmutableList<T> ofCollection(Collection<? extends T> collection) {
+        return collection.isEmpty() ? emptyList() : new ImmutableArrayList<>(collection);
     }
 
     @Nonnull
-    public static <T> ImmutableList<T> ofCollection(ReadOnlyCollection<? extends T> collection) {
+    static <T> ImmutableList<T> ofCollection(ReadOnlyCollection<? extends T> collection) {
         if (collection instanceof ImmutableList) {
-            //noinspection unchecked
-            return (ImmutableList<T>) collection;
+            @SuppressWarnings("unchecked")
+            ImmutableList<T> unchecked = (ImmutableList<T>) collection;
+            return unchecked;
         }
-        return collection.isEmpty() ? emptyList() : new ImmutableArrayList<T>(collection);
+        return collection.isEmpty() ? emptyList() : new ImmutableArrayList<>(collection);
     }
 
     @Nonnull
-    public static <T> ImmutableList<T> ofArray(@Nonnull Object[] a, int offset, int length) {
+    static <T> ImmutableList<T> ofArray(@Nonnull Object[] a, int offset, int length) {
         return length == 0 ? emptyList() : new ImmutableArrayList<>(a, offset, length);
     }
 
     @Nonnull
-    public static <T> ImmutableList<T> remove(@Nullable ReadOnlyCollection<T> collection, int index) {
+    static <T> ImmutableList<T> remove(@Nullable ReadOnlyCollection<T> collection, int index) {
         if (collection == null || collection.size() == 1 && index == 0) {
             return emptyList();
         }
@@ -113,7 +114,7 @@ public interface ImmutableList<E> extends ReadOnlyList<E>, ImmutableCollection<E
     }
 
     @Nonnull
-    public static <T> ImmutableList<T> remove(@Nullable Collection<T> collection, int index) {
+    static <T> ImmutableList<T> remove(@Nullable Collection<T> collection, int index) {
         if (collection == null || collection.size() == 1 && index == 0) {
             return emptyList();
         }
@@ -126,7 +127,7 @@ public interface ImmutableList<E> extends ReadOnlyList<E>, ImmutableCollection<E
     }
 
     @Nonnull
-    public static <T> ImmutableList<T> remove(@Nullable Collection<T> collection, T item) {
+    static <T> ImmutableList<T> remove(@Nullable Collection<T> collection, T item) {
         if (collection == null || collection.size() == 1 && collection.contains(item)) {
             return emptyList();
         }
@@ -141,20 +142,21 @@ public interface ImmutableList<E> extends ReadOnlyList<E>, ImmutableCollection<E
         }
     }
 
-    public static <T> ImmutableList<T> set(ReadOnlyCollection<T> collection, int index, T item) {
+    static <T> ImmutableList<T> set(ReadOnlyCollection<T> collection, int index, T item) {
         Object[] a = new Object[collection.size()];
         a = collection.toArray(a);
         a[index] = item;
         return new ImmutableArrayList<>(true, a);
     }
 
-    public static <T> ImmutableList<T> set(Collection<T> collection, int index, T item) {
+    static <T> ImmutableList<T> set(Collection<T> collection, int index, T item) {
         Object[] a = new Object[collection.size()];
         a = collection.toArray(a);
         a[index] = item;
         return new ImmutableArrayList<>(true, a);
     }
-    public static <E> ImmutableList<E> removeAll(ReadOnlyCollection<E> list, Collection<? extends E> collection) {
+
+    static <E> ImmutableList<E> removeAll(ReadOnlyCollection<E> list, Collection<? extends E> collection) {
         int n = list.size();
         Object[] a = new Object[n];
         int j = 0;
@@ -164,6 +166,6 @@ public interface ImmutableList<E> extends ReadOnlyList<E>, ImmutableCollection<E
             }
         }
 
-        return new ImmutableArrayList<E>(a, 0, j);
+        return new ImmutableArrayList<>(a, 0, j);
     }
 }
