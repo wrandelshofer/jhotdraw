3531526cba5e157cb17be423faca22a0e70ef7ff
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2019-02-03 20:15:42 +0100

    Fixes comments.

--- jhotdraw8/src/main/java/org/jhotdraw8/draw/figure/TextableFigure.java
+++ jhotdraw8/src/main/java/org/jhotdraw8/draw/figure/TextableFigure.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Labeled;
 import javafx.scene.text.Text;
 import org.jhotdraw8.annotation.Nonnull;
 import org.jhotdraw8.annotation.Nullable;
-
 import org.jhotdraw8.draw.key.DirtyBits;
 import org.jhotdraw8.draw.key.DirtyMask;
 import org.jhotdraw8.draw.key.StringStyleableFigureKey;
@@ -31,7 +30,7 @@ public interface TextableFigure extends Figure {
     /**
      * Updates a text node with textable properties.
      *
-     * @param ctx
+     * @param ctx the render context
      * @param text a text node
      */
     default void applyTextableFigureProperties(@Nullable RenderContext ctx, @Nonnull Text text) {
