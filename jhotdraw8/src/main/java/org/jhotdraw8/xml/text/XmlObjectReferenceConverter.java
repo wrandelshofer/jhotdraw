6af60a19046d71124a3dc1f54af02874d109d9ba
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2019-02-10 09:08:09 +0100

    Fixes annotations. Prints log output instead of console output.

--- jhotdraw8/src/main/java/org/jhotdraw8/xml/text/XmlObjectReferenceConverter.java
+++ jhotdraw8/src/main/java/org/jhotdraw8/xml/text/XmlObjectReferenceConverter.java
@@ -3,54 +3,61 @@
  */
 package org.jhotdraw8.xml.text;
 
+import org.jhotdraw8.annotation.Nonnull;
+import org.jhotdraw8.annotation.Nullable;
+import org.jhotdraw8.io.IdFactory;
+import org.jhotdraw8.text.Converter;
+
 import java.io.IOException;
 import java.nio.CharBuffer;
 import java.text.ParseException;
-
-import org.jhotdraw8.annotation.Nonnull;
-import org.jhotdraw8.annotation.Nullable;
-
-import org.jhotdraw8.io.IdFactory;
-import org.jhotdraw8.text.Converter;
+import java.util.logging.Logger;
 
 /**
  * XmlObjectReferenceConverter.
  * <p>
  * Converts references to figures.
  *
- * @author Werner Randelshofer
  * @param <T> the type
+ * @author Werner Randelshofer
  */
 public class XmlObjectReferenceConverter<T> implements Converter<T> {
 
-    @Nullable
+    private static final Logger LOGGER = Logger.getLogger(XmlObjectReferenceConverter.class.getName());
+    @Nonnull
     private final Class<T> clazz;
 
-    /** 
+    /**
      * Creates a new instance
+     *
      * @param clazz the type class
      * @throws IllegalArgumentException if clazz is null
      */
-    public XmlObjectReferenceConverter(@Nullable Class<T> clazz) {
-        if (clazz==null)throw new IllegalArgumentException("clazz is null");
+    public XmlObjectReferenceConverter(@Nonnull Class<T> clazz) {
         this.clazz = clazz;
     }
 
     @Override
-    public <TT extends T> void toString(@Nonnull Appendable out, @Nonnull IdFactory idFactory, @Nullable TT value) throws IOException {
+    public <TT extends T> void toString(@Nonnull Appendable out, @Nullable IdFactory idFactory, @Nullable TT value) throws IOException {
+        if (idFactory == null) {
+            throw new UnsupportedOperationException("idFactory is required for this converter");
+        }
         out.append(value == null ? "none" : idFactory.getId(value));
     }
 
     @Nullable
     @Override
-    public T fromString(@Nullable CharBuffer buf, @Nonnull IdFactory idFactory) throws ParseException, IOException {
-        String str = buf.toString();
+    public T fromString(@Nullable CharBuffer buf, @Nullable IdFactory idFactory) throws ParseException, IOException {
+        if (idFactory == null) {
+            throw new UnsupportedOperationException("idFactory is required for this converter");
+        }
+        String str = buf == null ? "none" : buf.toString();
         if ("none".equals(str)) {
             return null;
         }
         Object obj = idFactory.getObject(str);
         if (obj == null) {
-            System.err.println("WARNING Could not find an object with this id. id=\""+str+"\".");
+            LOGGER.warning("Could not find an object with this id. id=\"" + str + "\".");
         }
 
         @SuppressWarnings("unchecked")
