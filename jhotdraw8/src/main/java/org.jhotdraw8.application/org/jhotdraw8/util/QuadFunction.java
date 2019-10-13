b7066b22399fa03e427458dbc4ac36e002eac31d
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2019-10-13 14:05:22 +0200

    Adds QuadFunction. Fixes Javadoc of TriFunction.

--- jhotdraw8/src/main/java/org.jhotdraw8.application/org/jhotdraw8/util/QuadFunction.java
+++ jhotdraw8/src/main/java/org.jhotdraw8.application/org/jhotdraw8/util/QuadFunction.java
@@ -5,23 +5,25 @@
 package org.jhotdraw8.util;
 
 /**
- * Represents a function that accepts 3 arguments and produces a result.
+ * Represents a function that accepts 4 arguments and produces a result.
  *
  * @param <T> the type of the first argument to the function
  * @param <U> the type of the second argument to the function
  * @param <V> the type of the third argument to the function
+ * @param <W> the type of the fourth argument to the function
  * @param <R> the type of the result of the function
  */
 @FunctionalInterface
-public interface TriFunction<T, U, V, R> {
+public interface QuadFunction<T, U, V, W, R> {
     /**
      * Applies this function to the given arguments.
      *
      * @param t the first function argument
      * @param u the second function argument
      * @param v the third function argument
+     * @param w the fourth function argument
      * @return the function result
      */
-    R apply(T t, U u, V v);
+    R apply(T t, U u, V v, W w);
 
 }
