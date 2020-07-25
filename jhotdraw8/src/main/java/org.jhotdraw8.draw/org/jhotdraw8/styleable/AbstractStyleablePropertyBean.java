15f578dfd21736dcab610c2b36ef59cc31bf5e95
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2020-07-25 12:00:10 +0200

    Fixes atomic value.

--- jhotdraw8/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/styleable/AbstractStyleablePropertyBean.java
+++ jhotdraw8/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/styleable/AbstractStyleablePropertyBean.java
@@ -28,31 +28,32 @@ public abstract class AbstractStyleablePropertyBean implements StyleableProperty
     /**
      * Holds the properties.
      */
-    protected final StyleableMap<Key<?>, Object> properties = new SimpleStyleableMap<Key<?>, Object>(
-            keyMaps.computeIfAbsent(getClass(), k -> {
-                ConcurrentHashMap<Key<?>, Integer> m = new ConcurrentHashMap<Key<?>, Integer>() {
-                    @NonNull
-                    final AtomicInteger count = new AtomicInteger();
+    protected final StyleableMap<Key<?>, Object> properties =
+            new SimpleStyleableMap<Key<?>, Object>(
+                    keyMaps.computeIfAbsent(getClass(), k -> {
+                        ConcurrentHashMap<Key<?>, Integer> m = new ConcurrentHashMap<Key<?>, Integer>() {
+                            @NonNull
+                            final AtomicInteger nextIndex = new AtomicInteger();
 
-                    @Override
-                    public Integer get(Object key) {
-                        return super.computeIfAbsent((Key<?>) key, k -> count.incrementAndGet());
-                    }
-                };
+                            @Override
+                            public Integer get(Object key) {
+                                return super.computeIfAbsent((Key<?>) key, k -> nextIndex.getAndIncrement());
+                            }
+                        };
 
-                return m;
-            })
-    ) {
+                        return m;
+                    })
+            ) {
 
-        @Override
-        @SuppressWarnings("unchecked")
-        protected void callObservers(StyleOrigin origin, @NonNull MapChangeListener.Change<Key<?>, Object> change) {
-            changed((Key<Object>) change.getKey(), change.getValueRemoved(), change.getValueAdded());
-            AbstractStyleablePropertyBean.this.callObservers(origin, false, change);
-            super.callObservers(origin, change);
-        }
+                @Override
+                @SuppressWarnings("unchecked")
+                protected void callObservers(StyleOrigin origin, @NonNull MapChangeListener.Change<Key<?>, Object> change) {
+                    changed((Key<Object>) change.getKey(), change.getValueRemoved(), change.getValueAdded());
+                    AbstractStyleablePropertyBean.this.callObservers(origin, false, change);
+                    super.callObservers(origin, change);
+                }
 
-    };
+            };
 
     /**
      * Returns the user properties.
