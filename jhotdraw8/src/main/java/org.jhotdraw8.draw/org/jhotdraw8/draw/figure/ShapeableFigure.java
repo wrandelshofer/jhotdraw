8aef0621a987b5f37255ba1997ada907e190e72e
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2020-08-14 14:27:50 +0200

    ShapeableFigure: Reverts non-working changes in previous commit, but uses now a Path2D.Double for higher precision.

--- jhotdraw8/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/draw/figure/ShapeableFigure.java
+++ jhotdraw8/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/draw/figure/ShapeableFigure.java
@@ -7,6 +7,7 @@ package org.jhotdraw8.draw.figure;
 import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
 import javafx.scene.shape.Path;
+import javafx.scene.shape.PathElement;
 import org.jhotdraw8.annotation.NonNull;
 import org.jhotdraw8.css.CssSize;
 import org.jhotdraw8.draw.key.CssInsetsStyleableMapAccessor;
@@ -15,13 +16,15 @@ import org.jhotdraw8.draw.key.DoubleStyleableKey;
 import org.jhotdraw8.draw.key.NullableSvgPathStyleableKey;
 import org.jhotdraw8.draw.key.Rectangle2DStyleableMapAccessor;
 import org.jhotdraw8.draw.render.RenderContext;
+import org.jhotdraw8.geom.AWTPathBuilder;
 import org.jhotdraw8.geom.FXGeom;
 import org.jhotdraw8.geom.FXPathBuilder;
 import org.jhotdraw8.geom.NineRegionsScalingBuilder;
 import org.jhotdraw8.geom.Shapes;
 
+import java.awt.geom.Path2D;
 import java.text.ParseException;
-import java.util.logging.Logger;
+import java.util.List;
 
 public interface ShapeableFigure extends Figure {
     @NonNull CssSizeStyleableKey SHAPE_SLICE_BOTTOM = new CssSizeStyleableKey("shapeSliceBottom", CssSize.ZERO);
@@ -65,21 +68,26 @@ public interface ShapeableFigure extends Figure {
             content = SVG_SQUARE;
         }
 
-        FXPathBuilder builder = new FXPathBuilder(node.getElements());
         try {
+            AWTPathBuilder builder = new AWTPathBuilder(new Path2D.Double());
             Shapes.buildFromSvgString(builder, content);
+            Path2D path = builder.build();
+
+
+            javafx.geometry.Rectangle2D shapeBounds = getStyled(SHAPE_BOUNDS);
+
+            final Bounds srcBounds = shapeBounds == null || FXGeom.isEmpty(shapeBounds) ? FXGeom.getBounds(path) : FXGeom.getBounds(shapeBounds);
+            Insets shapeSlice = getStyledNonNull(SHAPE_SLICE).getConvertedValue(srcBounds.getWidth(), srcBounds.getHeight());
+
+            FXPathBuilder builder2 = new FXPathBuilder();
+            final NineRegionsScalingBuilder nineRegionsScalingBuilder = new NineRegionsScalingBuilder(builder2, srcBounds, shapeSlice, b);
+
+            Shapes.buildFromPathIterator(nineRegionsScalingBuilder, path.getPathIterator(null));
+            List<PathElement> elements = builder2.getElements();
+            node.getElements().setAll(elements);
+            node.setVisible(true);
         } catch (ParseException ex) {
-            Logger.getLogger(ShapeableFigure.class.getName()).warning("Illegal path: " + content);
+            node.setVisible(false);
         }
-        builder.getElements(); // will update the provided elements
-
-
-        javafx.geometry.Rectangle2D shapeBounds = getStyled(SHAPE_BOUNDS);
-
-        final Bounds srcBounds = shapeBounds == null || FXGeom.isEmpty(shapeBounds) ? node.getLayoutBounds() : FXGeom.getBounds(shapeBounds);
-        Insets shapeSlice = getStyledNonNull(SHAPE_SLICE).getConvertedValue(srcBounds.getWidth(), srcBounds.getHeight());
-
-        FXPathBuilder builder2 = new FXPathBuilder();
-        final NineRegionsScalingBuilder nineRegionsScalingBuilder = new NineRegionsScalingBuilder(builder2, srcBounds, shapeSlice, b);
     }
 }
